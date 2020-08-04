ccec91d90b2ee2af8eab56243aac15d5b515e79b
pvanliefland <pierre.vanliefland@gmail.com>
2020-08-04 22:14:23 +0200

    Simple solution for large files in s3 (#1)

--- jupyter/local_modules/habari/contents/s3manager.py
+++ jupyter/local_modules/habari/contents/s3manager.py
@@ -1,5 +1,10 @@
 from s3contents import S3ContentsManager as BaseS3ContentsManager
 
+from .largefiles import save as save_override
+
 
 class S3ContentsManager(BaseS3ContentsManager):
     """Placeholder for overriding S3ContentsManager"""
+
+    def save(self, model, path):
+        return save_override(BaseS3ContentsManager, self, model, path)
