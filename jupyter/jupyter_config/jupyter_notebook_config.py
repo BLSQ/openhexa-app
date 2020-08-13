df486c1314dc49798469a6d028563185f49d55f0
pvanliefland <pierre.vanliefland@gmail.com>
2020-08-13 11:38:11 +0200

    Minor config fix

--- jupyter/jupyter_config/jupyter_notebook_config.py
+++ jupyter/jupyter_config/jupyter_notebook_config.py
@@ -21,6 +21,4 @@ c.MultiContentsManager.manager_kwargs = {
     },
 }
 
-c.GCSManager.bucket = os.environ["GCS_BUCKET_NAME"]
-
 c.NotebookApp.tornado_settings = {"autoreload": os.environ.get("DEBUG", False)}
