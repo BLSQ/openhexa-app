86b2462c12df9ae19e294f7436d7431017e36b93
pvanliefland <pierre.vanliefland@gmail.com>
2020-08-20 10:21:38 +0200

    #16 Basic deploy automation
    Add scripts to build, deploy and teardown + adapted README

---
+++ bin/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+project=$1
+
+if [ "$project" = "" ]; then
+	echo "Please provide a project name"
+	exit 1
+fi
+
+echo "Deploying project $project"
+
+# TODO: --create-namespace should be an option
+helm upgrade --install "habari-$project" jupyterhub/jupyterhub \
+  --namespace "$project" \
+  --create-namespace \
+  --version=0.9.0 \
+  --values config.yaml \
+  --values config/"$project".yaml
+
+hub_ip=$(kubectl get svc proxy-public --namespace="$project" -o jsonpath='{.spec.clusterIP}')
+
+printf "\n"
+echo "Done."
+echo "Hub deployed at $hub_ip"
