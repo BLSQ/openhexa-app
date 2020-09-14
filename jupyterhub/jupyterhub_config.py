65ab18582990c58ff488785323a3f7c9bde60149
Eric Darchis <darchis@pobox.com>
2020-09-14 15:32:41 +0200

    Try to force Voilà cull params
    Voilà starts a kernel for every version of the notebook but never stops
    them. This parameter attempts to stop the unused kernels

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -47,3 +47,11 @@ c.JupyterHub.authenticator_class = GitHubOAuthenticator
 
 # Use Postgres as the hub database
 c.JupyterHub.db_url = os.environ["HUB_DB_URL"]
+
+# shutdown the server after no activity for an hour
+#c.NotebookApp.shutdown_no_activity_timeout = 60 * 60
+# shutdown kernels after no activity for 20 minutes
+c.MappingKernelManager.cull_idle_timeout = 20 * 60
+# check for idle kernels every two minutes
+c.MappingKernelManager.cull_interval = 2 * 60
+
