af799e5bba609ba9a9bed0c6202326d5bb0bb0da
pvanliefland <pierre.vanliefland@gmail.com>
2020-12-05 09:53:47 +0100

    HAB-64 Data catalog mockup screens
    Detail & breadcrumbs

--- platform/habari/catalog/urls.py
+++ platform/habari/catalog/urls.py
@@ -6,4 +6,5 @@ app_name = "catalog"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('<str:datasource_id>/', views.detail, name='detail')
 ]
--- platform/habari/catalog/views.py
+++ platform/habari/catalog/views.py
@@ -6,7 +6,8 @@ from django.shortcuts import render
 
 SAMPLE_DATASOURCES = [
     {
-        "type": {"code": "dhis2", "label": "DHIS2",},
+        "id": "aecc5e5e-d323-4398-9b86-5871afdd500f",
+        "type": {"code": "dhis2", "label": "DHIS2", },
         "name": "A dhis2 instance",
         "source": {
             "name": "A ministry of health somewhere",
@@ -18,7 +19,8 @@ SAMPLE_DATASOURCES = [
         "update_frequency": {"code": "daily", "label": "Updated daily"},
     },
     {
-        "type": {"code": "dhis2", "label": "DHIS2",},
+        "id": "831cfa1b-f8ec-474b-a3c9-da8ca58684be",
+        "type": {"code": "dhis2", "label": "DHIS2", },
         "name": "Another dhis2 instance",
         "source": {"name": "A partner NGO", "type": {"code": "ngo", "label": "NGO"}},
         "datasets_count": 2,
@@ -27,7 +29,8 @@ SAMPLE_DATASOURCES = [
         "update_frequency": {"code": "weekly", "label": "Updated weekly"},
     },
     {
-        "type": {"code": "iaso", "label": "IASO",},
+        "id": "b4de249d-449a-4a9c-a39b-ea2e83bd8dc5",
+        "type": {"code": "iaso", "label": "IASO", },
         "name": "A Iaso project",
         "source": {
             "name": "SNIS something",
@@ -39,7 +42,8 @@ SAMPLE_DATASOURCES = [
         "update_frequency": {"code": "daily", "label": "Updated daily"},
     },
     {
-        "type": {"code": "excel", "label": "Excel file",},
+        "id": "dcac5818-0f1a-4f52-98f2-4d514b7d7725",
+        "type": {"code": "excel", "label": "Excel file", },
         "name": "Running out of ideas here",
         "source": {
             "name": "An academic partner",
@@ -51,7 +55,8 @@ SAMPLE_DATASOURCES = [
         "update_frequency": {"code": "adhoc", "label": "Ad-hoc updates"},
     },
     {
-        "type": {"code": "csv", "label": "CSV file",},
+        "id": "14934a3d-e6c1-40c1-85a7-be099e5f7689",
+        "type": {"code": "csv", "label": "CSV file", },
         "name": "Seriously, help me",
         "source": {
             "name": "World health organization",
@@ -67,3 +72,9 @@ SAMPLE_DATASOURCES = [
 
 def index(request):
     return render(request, "catalog/index.html", {"datasources": SAMPLE_DATASOURCES})
+
+
+def detail(request, datasource_id):
+    datasource = next(ds for ds in SAMPLE_DATASOURCES if ds["id"] == datasource_id)
+
+    return render(request, "catalog/detail.html", {"datasource": datasource})
