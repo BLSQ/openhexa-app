135a8bbe04035646f466ded40c67460e58dfe14c
pvanliefland <pierre.vanliefland@gmail.com>
2021-02-04 10:15:59 +0100

    HAB-106 Users should authenticate before accessing the platform
    Fix static mess

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "habari/static"
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [BASE_DIR / "habari" / "static"]
 
 # Tailwind
 TAILWIND_APP_NAME = "habari.tailwindui"
