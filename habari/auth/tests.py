a494836225bf9a890050a730121ce340d0cf65f4
pvanliefland <pierre.vanliefland@gmail.com>
2021-02-04 13:04:01 +0100

    HAB-106 Users should authenticate before accessing the platform
    Auth tests

--- habari/auth/tests.py
+++ habari/auth/tests.py
@@ -1,5 +1,6 @@
-from django.conf import settings
 from django import test
+from django.conf import settings
+from django.urls import reverse
 
 from habari.auth.models import User
 
@@ -11,25 +12,28 @@ class SimpleTest(test.TestCase):
             "regular@bluesquarehub.com", "regular@bluesquarehub.com", "regular",
         )
 
-    def test_index_redirects_to_login(self):
-        response = self.client.get("/")
+    def test_any_page_anonymous_302(self):
+        response = self.client.get(reverse("dashboard:index"))
 
         # Check that the response is temporary redirection to /login.
         self.assertEqual(response.status_code, 302)
-        self.assertIn("/auth/login", response.url)
+        self.assertIn(settings.LOGIN_URL, response.url)
+
+    def test_logout_302(self):
+        self.client.login(email="regular@bluesquarehub.com", password="regular")
+        response = self.client.get(reverse("auth:logout"))
+
+        # Check that the response is temporary redirection to /login.
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(settings.LOGIN_URL, response.url)
 
     def test_login_200(self):
-        response = self.client.get(settings.LOGIN_URL)
+        response = self.client.get(reverse("auth:login"))
 
         self.assertEqual(response.status_code, 200)
 
-    def test_account_302(self):
-        response = self.client.get("/auth/account/")
-
-        self.assertEqual(response.status_code, 302)
-
     def test_account_200(self):
         self.client.login(email="regular@bluesquarehub.com", password="regular")
-        response = self.client.get("/auth/account/")
+        response = self.client.get(reverse("auth:account"))
 
         self.assertEqual(response.status_code, 200)
