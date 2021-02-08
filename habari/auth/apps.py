f9c0de2ca43b833a917c3356a92cafde2e5f516b
pvanliefland <pierre.vanliefland@gmail.com>
2021-02-08 22:46:10 +0100

    Explanation of auth app label

--- habari/auth/apps.py
+++ habari/auth/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 class AuthConfig(AppConfig):
     name = "habari.auth"
-    label = "habari_auth"
+    label = "habari_auth"  # avoid conflicts with base django auth
 
     verbose_name = "User management"
