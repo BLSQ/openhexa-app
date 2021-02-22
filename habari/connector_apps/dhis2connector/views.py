e4962e2accd2ec467ccc2595ff354cc100546789
pvanliefland <pierre.vanliefland@gmail.com>
2021-02-22 19:53:36 +0100

    HEXA-140 Allow users to view DHIS2 data elements
    List views

--- habari/connector_apps/dhis2connector/views.py
+++ habari/connector_apps/dhis2connector/views.py
@@ -1,25 +1,7 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from habari.catalog.models import Datasource
-
-
-def data_element_list(request, datasource_id):
-    datasource = get_object_or_404(Datasource, pk=datasource_id)
-
-    breadcrumbs = [
-        ("Catalog", "catalog:index"),
-        (datasource.display_name, "dhis2connector:datasource_detail", datasource_id),
-        ("Data Elements", "foo:bar"),
-    ]
-
-    return render(
-        request,
-        "dhis2connector/data_element_list.html",
-        {
-            "datasource": datasource,
-            "breadcrumbs": breadcrumbs,
-        },
-    )
 
 
 def datasource_detail(request, datasource_id):
@@ -36,8 +18,9 @@ def datasource_detail(request, datasource_id):
         {
             "datasource": datasource,
             "data_elements_summary_params": {
-                "item_name": "Data element",  # TODO: translate
-                "item_set": datasource.dhis2dataelement_set,
+                "title": "Data elements",  # TODO: translate
+                "item_name": "data element",  # TODO: translate
+                "page": Paginator(datasource.dhis2dataelement_set.all(), 5).page(1),
                 "columns": [
                     "Name",
                     "Code",
@@ -46,11 +29,12 @@ def datasource_detail(request, datasource_id):
                     "Last update",
                 ],
                 "list_url": "dhis2connector:data_element_list",
-                "item_summary_template": "dhis2connector/partials/data_element_summary_item.html",
+                "summary_template": "dhis2connector/partials/data_element_summary_item.html",
             },
             "indicator_summary_params": {
-                "item_name": "Indicator",  # TODO: translate
-                "item_set": datasource.dhis2indicator_set,
+                "title": "Indicators",  # TODO: translate
+                "item_name": "indicator",  # TODO: translate
+                "page": Paginator(datasource.dhis2indicator_set.all(), 5).page(1),
                 "columns": [
                     "Name",
                     "Code",
@@ -59,7 +43,42 @@ def datasource_detail(request, datasource_id):
                     "Last update",
                 ],
                 "list_url": "dhis2connector:data_element_list",
-                "item_summary_template": "dhis2connector/partials/indicator_summary_item.html",
+                "summary_template": "dhis2connector/partials/indicator_summary_item.html",
+            },
+            "breadcrumbs": breadcrumbs,
+        },
+    )
+
+
+def data_element_list(request, datasource_id, page_number=1):
+    datasource = get_object_or_404(Datasource, pk=datasource_id)
+    paginator = Paginator(datasource.dhis2dataelement_set.all(), 10)
+    page = paginator.page(page_number)
+
+    breadcrumbs = [
+        ("Catalog", "catalog:index"),
+        (datasource.display_name, "dhis2connector:datasource_detail", datasource_id),
+        ("Data Elements", "foo:bar"),
+    ]
+
+    return render(
+        request,
+        "dhis2connector/data_element_list.html",
+        {
+            "datasource": datasource,
+            "data_elements_paginated_params": {
+                "title": "Data elements",  # TODO: translate
+                "label": f"{page.start_index()} to {page.end_index()} out of {paginator.count}",  # TODO: translate
+                "item_name": "data element",  # TODO: translate
+                "page": page,
+                "columns": [
+                    "Name",
+                    "Code",
+                    "Values",
+                    "Tags",
+                    "Last update",
+                ],
+                "summary_template": "dhis2connector/partials/data_element_summary_item.html",
             },
             "breadcrumbs": breadcrumbs,
         },
