41d90568d1016c42a010866bb4513a83adace717
pvanliefland <pierre.vanliefland@gmail.com>
2021-03-10 11:04:31 +0100

    HEXA-182 Refactor datasource and searchable content
    Admin fine-tuning

--- habari/catalog/admin.py
+++ habari/catalog/admin.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
-from .models import Datasource
-from ..user_management.models import Organization
 
-
-def country_list(obj):
-    return ",".join(country.alpha3 for country in obj.countries)
-
-
-country_list.short_description = "Countries"
-
-
-@admin.register(Organization)
-class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ("display_name", "organization_type", country_list)
+from habari.catalog.models import Datasource
+from habari.common.admin import country_list
 
 
 @admin.register(Datasource)
