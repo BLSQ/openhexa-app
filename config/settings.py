34f4dca041890750c9695b1d85fc2b3ea538eff9
pvanliefland <pierre.vanliefland@gmail.com>
2021-03-19 14:39:51 +0100

    HEXA-83 Platform provisioning and CD
    Simpler approach (no envsubst)

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "false") == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+if not DEBUG:
+    ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 
