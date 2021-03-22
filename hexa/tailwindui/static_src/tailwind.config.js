438f9b67efb307c44fec2a47b3f7e7ba786a85e1
pvanliefland <pierre.vanliefland@gmail.com>
2021-03-22 13:39:01 +0100

    HEXA-136 Update django-tailwind
    Finish update

--- hexa/tailwindui/static_src/tailwind.config.js
+++ hexa/tailwindui/static_src/tailwind.config.js
@@ -1,5 +1,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+// This is a minimal config.
+// If you need the full config, get it from here:
+// https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
 module.exports = {
     purge: [],  // TODO: https://github.com/timonweb/django-tailwind#purgecss-setup
     darkMode: false, // or 'media' or 'class'
