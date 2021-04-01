736cf4bd73db162a9c7cc621ac8ddc45141402d0
pvanliefland <pierre.vanliefland@gmail.com>
2021-04-01 10:42:59 +0200

    HEXA-188 S3 dynamic mounts in notebooks
    Plugin app setup

---
+++ hexa/plugins/connector_s3/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+
+class ConnectorS3Config(AppConfig):
+    name = "hexa.plugins.connector_s3"
+    label = "connector_s3"
+
+    verbose_name = "S3 Connector"
+
+    @property
+    def route_prefix(self):
+        return "s3"
