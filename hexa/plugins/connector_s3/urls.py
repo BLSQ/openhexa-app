7fca65d70813625bab35848a94b8967c9a6f2826
pvanliefland <pierre.vanliefland@gmail.com>
2021-04-12 14:35:26 +0200

    HEXA-188 S3 dynamic mounts in notebooks
    URL cleanup

--- hexa/plugins/connector_s3/urls.py
+++ hexa/plugins/connector_s3/urls.py
@@ -6,11 +6,6 @@ app_name = "connector_s3"
 
 urlpatterns = [
     path("<str:datasource_id>", views.datasource_detail, name="datasource_detail"),
-    # path(
-    #     "<str:datasource_id>/data-elements/",
-    #     views.data_element_list,
-    #     name="data_element_list",
-    # ),
     path(
         "<str:datasource_id>/sync",
         views.datasource_sync,
