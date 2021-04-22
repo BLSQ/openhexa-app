cf4304c0e4753ac3fda29c80f12fedc4c326fa06
pvanliefland <pierre.vanliefland@gmail.com>
2021-04-22 10:55:39 +0200

    Fix embed template tag (to play nice with extends and allow multiple embeds of the same template)

--- hexa/core/templatetags/embed.py
+++ hexa/core/templatetags/embed.py
@@ -1,38 +1,54 @@
+import uuid
+
 from django import template as django_template
-from django.template.loader_tags import (
-    ExtendsNode,
-    do_include,
-)
+from django.template.base import Node
+from django.template import loader_tags
 
 register = django_template.Library()
 
 
-class EmbedNode(ExtendsNode):
+class EmbedNode(Node):
     """Embedding a template is a lot like extending it, with a few differences:
     - The node list comes from the nodes found between {% embed %} and {% endembed %}
     - We override render() to take the "with" and "only" arguments into account, like for {% include %}
     """
 
     must_be_first = False
-    context_key = "embed_context"
 
     def __init__(
         self, nodelist, embedded_name, extra_context=None, isolated_context=False
     ):
+        self.nodelist = nodelist
+        self.embedded_name = embedded_name
         self.extra_context = extra_context or {}
         self.isolated_context = isolated_context
-        super().__init__(nodelist, embedded_name)
 
     def render(self, context):
         values = {
             name: var.resolve(context) for name, var in self.extra_context.items()
         }
 
-        if self.isolated_context:
-            return super().render(context.new(values))
+        # Create an extend node with the same origin and use it for rendering
+        extends_node = loader_tags.ExtendsNode(self.nodelist, self.embedded_name)
+        extends_node.origin = getattr(self, "origin")
 
-        with context.push(**values):
-            return super().render(context)
+        # We need to generate new context keys for the extends node (to avoid history issues) and temporarily override
+        # the value of BLOCK_CONTEXT key (to allow the sample template to be rendered twice with different block
+        # contexts)
+        extends_node.context_key = f"embed_context_{uuid.uuid4()}"
+        block_context_key_value = loader_tags.BLOCK_CONTEXT_KEY
+        loader_tags.BLOCK_CONTEXT_KEY = f"block_context_{uuid.uuid4()}"
+
+        if self.isolated_context:
+            rendered_node = extends_node.render(context.new(values))
+        else:
+            with context.push(**values):
+                rendered_node = extends_node.render(context)
+
+        # Restore BLOCK_CONTEXT_KEY to its previous value
+        loader_tags.BLOCK_CONTEXT_KEY = block_context_key_value
+
+        return rendered_node
 
 
 @register.tag("embed")
@@ -49,12 +65,19 @@ def do_embed(parser, token):
     You may use the ``only`` argument and keyword arguments using ``with`` like when using ``{% include %}``
     """
 
+    # do_block() will raise an exception if it detects two blocks with the same name
+    current_loaded_blocks = getattr(parser, "__loaded_blocks", [])
+    setattr(parser, "__loaded_blocks", [])
+
     nodelist = parser.parse(("endembed",))
     parser.delete_first_token()
 
     # This is a bit of a hack, but it allows to benefit from the parsing done for the {% include %} tag
-    # without dulicating code
-    include_node = do_include(parser, token)
+    # without duplicating code
+    include_node = loader_tags.do_include(parser, token)
+
+    # now we can reassign the __loaded_blocks to its previous value
+    setattr(parser, "__loaded_blocks", current_loaded_blocks)
 
     return EmbedNode(
         nodelist,
--- hexa/core/tests/test_templatetags.py
+++ hexa/core/tests/test_templatetags.py
@@ -16,13 +16,21 @@ class TemplatetagsTest(SimpleTestCase):
         return self.assertEqual(remove_whitespace(first), remove_whitespace(second))
 
     def test_embed_simple(self):
-        context = Context({"title": "Context Title"})
+        """Simplest case, single embed on a template that does not extend anything"""
+
+        context = Context(
+            {"title_1": "First Embedded Title", "title_2": "Second Embedded Title"}
+        )
         template_to_render = Template(
             """
             {% load embed %}
             {% embed "section.html" %}
-                {% block title %}{{ title }}{% endblock %}
-                {% block content %}<p>Content</p>{% endblock %}
+                {% block title %}{{ title_1 }}{% endblock %}
+                {% block content %}<p>Some content</p>{% endblock %}
+            {% endembed %}
+            {% embed "section.html" %}
+                {% block title %}{{ title_2 }}{% endblock %}
+                {% block content %}<p>Another content</p>{% endblock %}
             {% endembed %}
             """,
             engine=ENGINE,
@@ -31,11 +39,57 @@ class TemplatetagsTest(SimpleTestCase):
         self.assertTemplateContentEqual(
             """
             <section>
-                <h1>Context Title</h1>
+                <h1>First Embedded Title</h1>
                 <div>
-                    <p>Content</p>
+                    <p>Some content</p>
                 </div>
             </section>
+            <section>
+                <h1>Second Embedded Title</h1>
+                <div>
+                    <p>Another content</p>
+                </div>
+            </section>
+            """,
+            rendered_template,
+        )
+
+    def test_embed_extends(self):
+        """Make sure that embed does not conflict with extends."""
+
+        context = Context({"title": "Embedded Title"})
+        template_to_render = Template(
+            """
+            {% extends "base.html" %}
+            {% load embed %}
+            {% block page_title %}Extended Title{% endblock %}
+            {% block body %}
+                {% embed "section.html" %}
+                    {% block title %}{{ title }}{% endblock %}
+                {% block content %}<p>Content</p>{% endblock %}
+            {% endembed %}
+            {% endblock %}
+            """,
+            engine=ENGINE,
+        )
+        rendered_template = template_to_render.render(context)
+        self.assertTemplateContentEqual(
+            """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Extended Title</title>
+            </head>
+            <body>
+                <section>
+                    <h1>Embedded Title</h1>
+                    <div>
+                        <p>Content</p>
+                    </div>
+                </section>
+            </body>
+            </html>
             """,
             rendered_template,
         )
