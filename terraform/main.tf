bfe8513ef13cc979e5e1f0f8ad967ab519bd7225
pvanliefland <pierre.vanliefland@gmail.com>
2021-05-05 22:24:35 +0200

    HEXA-83 App provisioning and CD
    CSP fine-tuning

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,7 +76,7 @@ resource "google_service_account" "app_cloud_sql_proxy" {
   account_id   = var.gcp_iam_cloud_sql_proxy_service_account_id
   display_name = var.gcp_iam_cloud_sql_proxy_service_account_display_name
   project      = var.gcp_project_id
-  description  = "Used to allow pods to access Cloud SQL"
+  description  = "Cloud SQL Proxy"
 }
 resource "google_service_account_key" "app_cloud_sql_proxy" {
   service_account_id = google_service_account.app_cloud_sql_proxy.name
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -51,7 +51,7 @@ variable "gcp_sql_user_name" {
 # Service account for the Cloud SQL proxy
 variable "gcp_iam_cloud_sql_proxy_service_account_id" {
   description = "The ID of the service account used for the Cloud SQL proxy"
-  default     = "hexa-app-cloud-sql-proxy"
+  default     = "hexa-app-csp"
 }
 variable "gcp_iam_cloud_sql_proxy_service_account_display_name" {
   description = "The display name of the service account used for the Cloud SQL proxy"
