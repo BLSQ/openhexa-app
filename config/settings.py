fae8d6f9294fcda63c58820027e3e0e7f1827527
pvanliefland <pierre.vanliefland@gmail.com>
2021-05-10 13:43:14 +0200

    HEXA-121 Users should be able to request a data extract
    Extract UX POC

--- config/settings.py
+++ config/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "hexa.core.context_processors.global_variables",
+                "hexa.plugins.connector_dhis2.context_processors.current_extract",
             ],
         },
     },
--- hexa/plugins/connector_dhis2/urls.py
+++ hexa/plugins/connector_dhis2/urls.py
@@ -22,6 +22,11 @@ urlpatterns = [
         name="data_element_update",
     ),
     path(
+        "<str:instance_id>/data-elements/<str:data_element_id>/extract",
+        views.data_element_extract,
+        name="data_element_extract",
+    ),
+    path(
         "<str:instance_id>/indicators/",
         views.indicator_list,
         name="indicator_list",
@@ -37,8 +42,23 @@ urlpatterns = [
         name="indicator_update",
     ),
     path(
+        "<str:instance_id>/indicators/<str:indicator_id>/extract",
+        views.indicator_extract,
+        name="indicator_extract",
+    ),
+    path(
         "<str:instance_id>/sync",
         views.instance_sync,
         name="instance_sync",
+    ),
+    path(
+        "extract/<str:extract_id>",
+        views.extract_detail,
+        name="extract_detail",
+    ),
+    path(
+        "extract/<str:extract_id>/delete",
+        views.extract_delete,
+        name="extract_delete",
     ),
 ]
