ef4e4788c29f4a1fb14c6df5924dd2db993d4939
Mbayang <mdiop@bluesquarehub.com>
2021-05-19 09:30:58 +0000

    Adapt terraform_import

--- terraform_import/variables.tf
+++ terraform_import/variables.tf
@@ -14,3 +14,7 @@ variable "gcp_sql_instance_tier" {
   description = "The tier to use for the Cloud SQL instance"
   default     = "db-custom-1-3840"
 }
+variable "gcp_sql_instance_availability_type" {
+  description = "The availability type for the master instance.This is only used to set up high availability for the PostgreSQL instance. Can be either `ZONAL` or `REGIONAL`."
+  default     = "ZONAL"
+}
