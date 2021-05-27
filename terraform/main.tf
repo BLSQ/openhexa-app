3300d46ae7651a500fed515e12496fc097ff56be
pvanliefland <pierre.vanliefland@gmail.com>
2021-05-27 10:58:55 +0200

    HEXA-183 Secure storage of credentials added in the catalog
    Attempt to make app deployment depending on secrets and config maps

--- terraform/main.tf
+++ terraform/main.tf
@@ -218,7 +218,6 @@ resource "kubernetes_secret" "app" {
     DATABASE_PORT     = 5432
     SECRET_KEY        = random_password.django_secret_key.result
     ENCRYPTION_KEY    = "${random_id.django_encryption_key.b64_url}="
-    TEST_SECRET_VALUE = "test"
   }
 }
 
