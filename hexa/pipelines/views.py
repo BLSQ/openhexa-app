1b73aeadc04261b8da33179f08ce9dc57c8825f5
pvanliefland <pierre.vanliefland@gmail.com>
2021-05-27 15:38:39 +0200

    Pipelines UX fine-tuning + mock "last runs"

--- hexa/pipelines/views.py
+++ hexa/pipelines/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils.translation import ugettext_lazy as _
 
 from hexa.pipelines.models import PipelinesIndexType, PipelinesIndex
+from hexa.plugins.connector_airflow.models import DAGConfigRun, DAGConfigRunState
 
 
 def index(request):
@@ -12,11 +13,19 @@ def index(request):
         index_type=PipelinesIndexType.PIPELINES_ENVIRONMENT.value
     )
 
+    # TODO: replace by indexed runs
+    dag_config_runs = DAGConfigRun.objects.filter_for_user(request.user)
+
+    # TODO: actual refresh should be done using a CRON
+    for run in dag_config_runs.filter(airflow_state=DAGConfigRunState.RUNNING):
+        run.refresh()
+
     return render(
         request,
         "pipelines/index.html",
         {
             "environment_indexes": environment_indexes,
+            "dag_config_runs": dag_config_runs,
             "breadcrumbs": breadcrumbs,
         },
     )
