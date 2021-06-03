8b1681ce1f5f2b13c3378944e7160024b5e37c2d
pvanliefland <pierre.vanliefland@gmail.com>
2021-06-03 09:27:06 +0200

    HEXA-242 Issue with avatar / username in comments
    Fixed avatar and display name issue in comments

--- hexa/user_management/models.py
+++ hexa/user_management/models.py
@@ -34,6 +34,9 @@ class User(AbstractUser):
 
     @property
     def display_name(self):
+        if self.first_name or self.last_name:
+            return f"{self.first_name} {self.last_name}".strip()
+
         return self.username
 
     @property
