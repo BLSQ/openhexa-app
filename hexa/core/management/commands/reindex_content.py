7a0afe921ecd35615a345a15d95c894b4f2e0c86
pvanliefland <pierre.vanliefland@gmail.com>
2021-06-11 17:04:42 +0200

    HEXA-250 Index (permissions) duplicates
    Delete indexes before reindexing

--- hexa/core/management/commands/reindex_content.py
+++ hexa/core/management/commands/reindex_content.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
+from hexa.catalog.models import CatalogIndex
+from hexa.pipelines.models import PipelinesIndex
 from hexa.plugins.app import get_connector_app_configs
 
 
@@ -10,19 +12,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(f"Re-indexing...")
 
+        self.stdout.write("Deleting existing catalog indexes")
+        CatalogIndex.objects.all().delete()
+        self.stdout.write("Deleting existing pipelines indexes")
+        PipelinesIndex.objects.all().delete()
+
         for app_config in get_connector_app_configs():
-            app_models = list(app_config.get_models())
-            indexed_models = [
-                m
-                for m in app_models
-                if hasattr(m, "index") and callable(getattr(m, "index"))
+            app_model_classes = list(app_config.get_models())
+            indexed_model_classes = [
+                mc
+                for mc in app_model_classes
+                if hasattr(mc, "index") and callable(getattr(mc, "index"))
             ]
 
             with transaction.atomic():
-                self.stdout.write(f"Re-indexing content")
-                for indexed_model in indexed_models:
-                    for datasource in indexed_model.objects.all():
-                        self.stdout.write(f"Re-indexing {datasource}")
-                        datasource.save()
+                self.stdout.write(f"Re-indexing content for {app_config.label}")
+                for indexed_model_class in indexed_model_classes:
+                    for index_object in indexed_model_class.objects.all():
+                        self.stdout.write(f"Re-indexing {index_object}")
+                        index_object.save()
 
         self.stdout.write(self.style.SUCCESS("Done"))
