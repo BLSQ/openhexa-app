ae9942b0943237bc93d9aae901586e992cd1d82c
Nikita Marchant <nmarchant@bluesquarehub.com>
2021-07-22 12:27:39 +0200

    Add a test on the GraphQL endpoint for bucket and S3 object

--- hexa/plugins/connector_s3/tests/test_connector.py
+++ hexa/plugins/connector_s3/tests/test_connector.py
@@ -1,10 +1,32 @@
 from django import test
 from django.urls import reverse
 from unittest import skip
+import json
 
 from hexa.catalog.models import CatalogIndex
-from hexa.plugins.connector_s3.models import Credentials, Bucket, BucketPermission
-from hexa.user_management.models import User, Team, Membership
+from hexa.plugins.connector_s3.models import (
+    Credentials,
+    Bucket,
+    BucketPermission,
+    Object,
+)
+from hexa.user_management.models import User, Team, Membership, Organization
+
+
+def query(client, payload):
+    return json.loads(
+        client.post(
+            "/graphql/",
+            json.dumps(
+                {
+                    "operationName": None,
+                    "variables": {},
+                    "query": payload,
+                }
+            ),
+            content_type="application/json",
+        ).content
+    )
 
 
 class ConnectorS3Test(test.TestCase):
@@ -54,3 +76,146 @@ class ConnectorS3Test(test.TestCase):
         self.assertEqual(1, CatalogIndex.objects.filter(object_id=bucket_id).count())
         bucket.delete()
         self.assertEqual(0, CatalogIndex.objects.filter(object_id=bucket_id).count())
+
+    def test_graph(self):
+        self.maxDiff = None
+        self.client.login(email="jim@bluesquarehub.com", password="regular")
+        o = Organization.objects.create(name="Bluesquare")
+
+        level1 = Object.objects.create(
+            bucket=self.BUCKET,
+            s3_size=1234,
+            name="name1",
+            short_name="short1",
+            description="desc",
+            locale="en",
+            s3_key="/dir1",
+            s3_type="directory",
+            s3_storage_class="GLACIER",
+            s3_name="s3Name1",
+            owner=o,
+        )
+
+        level2 = Object.objects.create(
+            bucket=self.BUCKET,
+            s3_size=1234,
+            name="name2",
+            short_name="short2",
+            description="desc",
+            locale="en",
+            s3_key="/dir1/dir2/",
+            s3_type="directory",
+            s3_storage_class="GLACIER",
+            s3_name="s3Name2",
+            owner=o,
+            parent=level1,
+        )
+
+        Object.objects.create(
+            bucket=self.BUCKET,
+            s3_size=1234,
+            name="name3",
+            short_name="short3",
+            description="desc",
+            locale="en",
+            s3_key="/dir1/dir2/file1",
+            s3_type="directory",
+            s3_storage_class="GLACIER",
+            s3_name="s3Name3",
+            owner=o,
+            parent=level2,
+        )
+
+        r = query(
+            self.client,
+            """
+                query {
+                  s3Bucket(id: "%s") {
+                    Objects(page: 1) {
+                      items {
+                        owner {
+                          name
+                        }
+                        name
+                        shortName
+                        description
+                        countries {
+                          name
+                        }
+                        locale
+                        tags {
+                          name
+                        }
+                        bucket {
+                          name
+                        }
+                        parent {
+                          name
+                        }
+                        s3Key
+                        s3Size
+                        s3StorageClass
+                        s3Type
+                        s3Name
+                        Objects(page: 1) {
+                          items {
+                            name
+                            s3Key
+                            Objects(page: 1) {
+                              items {
+                                name
+                                s3Key
+                                Objects(page: 1) {
+                                  items {id
+                                    name
+                                    s3Key
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+            """
+            % self.BUCKET.id,
+        )
+
+        self.assertEquals(
+            r["data"]["s3Bucket"]["Objects"]["items"][0],
+            {
+                "owner": {"name": "Bluesquare"},
+                "name": "name1",
+                "shortName": "short1",
+                "description": "desc",
+                "countries": [],
+                "locale": "en",
+                "tags": [],
+                "bucket": {"name": ""},
+                "parent": None,
+                "s3Key": "/dir1",
+                "s3Size": 1234,
+                "s3StorageClass": "GLACIER",
+                "s3Type": "directory",
+                "s3Name": "s3Name1",
+                "Objects": {
+                    "items": [
+                        {
+                            "name": "name2",
+                            "s3Key": "/dir1/dir2/",
+                            "Objects": {
+                                "items": [
+                                    {
+                                        "name": "name3",
+                                        "s3Key": "/dir1/dir2/file1",
+                                        "Objects": {"items": []},
+                                    }
+                                ]
+                            },
+                        }
+                    ]
+                },
+            },
+        )
