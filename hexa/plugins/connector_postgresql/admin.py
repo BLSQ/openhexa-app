2085bd17c67c036324b7c81900cac1563b45ac69
Nikita Marchant <nmarchant@bluesquarehub.com>
2021-08-04 16:13:22 +0200

    Add missing PostgresqlDatabasePermissionAdmin

--- hexa/plugins/connector_postgresql/admin.py
+++ hexa/plugins/connector_postgresql/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import PostgresqlDatabase
+from .models import PostgresqlDatabase, PostgresqlDatabasePermission
 
 
 @admin.register(PostgresqlDatabase)
 class PostgresqlDatabaseAdmin(admin.ModelAdmin):
     list_display = ("database", "hostname", "port", "username")
+
+
+@admin.register(PostgresqlDatabasePermission)
+class PostgresqlDatabasePermissionAdmin(admin.ModelAdmin):
+    list_display = ("database", "team")
