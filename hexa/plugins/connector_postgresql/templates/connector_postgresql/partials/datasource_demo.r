e454b2739d9e476e9e6beedb680cc1f09ab54541
Nikita Marchant <nmarchant@bluesquarehub.com>
2021-08-19 16:58:16 +0200

    Fix wrong environment variable in the R example for PostgreSQL

--- hexa/plugins/connector_postgresql/templates/connector_postgresql/partials/datasource_demo.r
+++ hexa/plugins/connector_postgresql/templates/connector_postgresql/partials/datasource_demo.r
@@ -5,7 +5,7 @@ con <- dbConnect(
     dbname = Sys.getenv("{{ datasource.env_name }}_DATABASE"),
     host = Sys.getenv("{{ datasource.env_name }}_HOSTNAME"),
     port = Sys.getenv("{{ datasource.env_name }}_PORT"),
-    user = Sys.getenv("{{ datasource.env_name }}_USER"),
+    user = Sys.getenv("{{ datasource.env_name }}_USERNAME"),
     password = Sys.getenv("{{ datasource.env_name }}_PASSWORD")
 )
 
