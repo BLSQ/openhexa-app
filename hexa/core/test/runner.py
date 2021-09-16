616c5c928f2f973bebb7190f00a229ca82e14605
Pierre Vanliefland <pvanliefland@users.noreply.github.com>
2021-09-16 14:08:38 +0200

    Cache busting for js (#69)
    * HEXA-339 Cache busting for js
    
    Enable Whitenoise caching / cache busting
    
    * HEXA-339 Cache busting for js
    
    Black
    
    * HEXA-339 Cache busting for js
    
    Custom test runner to switch back to default static during test

---
+++ hexa/core/test/runner.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.test.runner import DiscoverRunner as BaseDiscoverRunner
+
+
+class DiscoverRunner(BaseDiscoverRunner):
+    def setup_test_environment(self, **kwargs):
+        super().setup_test_environment(**kwargs)
+        # ManifestStaticFileStorage & friends are not well-suited for tests, as they would required
+        # collectstatic to be run before each test run
+        settings.STATICFILES_STORAGE = (
+            "django.contrib.staticfiles.storage.StaticFilesStorage"
+        )
