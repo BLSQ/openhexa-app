77b6baa3a67d5f6c882a00b266a68696365327fb
pvanliefland <pierre.vanliefland@gmail.com>
2021-10-12 16:09:33 +0200

    Added Status property for datacard + test

--- hexa/plugins/connector_airflow/tests/test_views.py
+++ hexa/plugins/connector_airflow/tests/test_views.py
@@ -2,7 +2,7 @@ from django import test
 from django.urls import reverse
 from django.utils import timezone
 
-from hexa.plugins.connector_airflow.datacards import ClusterCard, DAGCard
+from hexa.plugins.connector_airflow.datacards import ClusterCard, DAGCard, DAGRunCard
 from hexa.plugins.connector_airflow.models import DAG, Cluster, DAGRun
 from hexa.user_management.models import User
 
@@ -51,3 +51,23 @@ class ViewsTest(test.TestCase):
         self.assertIsInstance(response.context["dag_card"], DAGCard)
         self.assertEqual(0, len(response.context["config_grid"]))
         self.assertEqual(2, len(response.context["run_grid"]))
+
+    def test_dag_run_detail_200(self):
+        cluster = Cluster.objects.create(
+            name="Test cluster", url="http://one-cluster-url.com"
+        )
+        dag = DAG.objects.create(cluster=cluster, dag_id="Test DAG")
+        dag_run = DAGRun.objects.create(dag=dag, execution_date=timezone.now())
+        self.client.force_login(self.USER_TAYLOR)
+        response = self.client.get(
+            reverse(
+                "connector_airflow:dag_run_detail",
+                kwargs={
+                    "cluster_id": cluster.id,
+                    "dag_id": dag.id,
+                    "dag_run_id": dag_run.id,
+                },
+            ),
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context["dag_run_card"], DAGRunCard)
