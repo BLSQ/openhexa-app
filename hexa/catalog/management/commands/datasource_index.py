aee03cbfee21ce166a7ffb570114c123816d9764
hastakecorp <github@corp.hastake.com>
2021-11-16 13:10:10 +0100

    [ref] HEXA-383 improve datasource/environment sync/index log output

--- hexa/catalog/management/commands/datasource_index.py
+++ hexa/catalog/management/commands/datasource_index.py
@@ -1,9 +1,12 @@
-import tqdm as tqdm
+from logging import getLogger
+
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
 from hexa.catalog.models import Datasource
 from hexa.plugins.app import ConnectorAppConfig
+
+logger = getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -16,15 +19,17 @@ class Command(BaseCommand):
         indexables = ConnectorAppConfig.get_models_by_capability("index", filter_app)
 
         for app, models in indexables.items():
-            print(f"- {app.verbose_name}")
             for model in models:
                 if not issubclass(model, Datasource):
                     # ignore index-able non datasource
                     continue
 
-                with transaction.atomic():
-                    instances = model.objects.all()
-                    pbar = tqdm.tqdm(instances)
-                    pbar.set_description(f"   {model.__name__:15}")
-                    for instance in pbar:
-                        instance.build_index()
+                for instance in model.objects.all():
+                    try:
+                        logger.info(
+                            "building index %s:%s", model, instance.display_name
+                        )
+                        with transaction.atomic():
+                            instance.build_index()
+                    except Exception:
+                        logger.exception("index error")
