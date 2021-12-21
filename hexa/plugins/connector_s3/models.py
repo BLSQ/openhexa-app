7fcc1d048d822b205aa70dbe8ee3566077412e3b
ekatsah <github@hastake.com>
2021-12-21 09:39:52 +0100

    directories may be object too -- filter them (#126)
    * [fix] directories may be object too -- filter them
    
    * [ref] naming
    
    Co-authored-by: hastakecorp <github@corp.hastake.com>

--- hexa/plugins/connector_s3/models.py
+++ hexa/plugins/connector_s3/models.py
@@ -146,14 +146,14 @@ class Bucket(Datasource):
         )
 
         # transform objects -> extract directories
-        directories, last_modified = set(), {}
+        directory_set, last_modified = set(), {}
         for object in objects:
             object["Type"] = "file"
             object["ETag"] = object["ETag"].strip('"')
             path = object["Key"].strip("/").split("/")
             if len(path) > 1:
                 dirname = "/".join(path[:-1]) + "/"
-                directories.add(dirname)
+                directory_set.add(dirname)
                 if (
                     dirname not in last_modified
                     or last_modified[dirname] < object["LastModified"]
@@ -169,8 +169,11 @@ class Bucket(Datasource):
                 "StorageClass": "DIRECTORY",
                 "Type": "directory",
             }
-            for name in directories
+            for name in directory_set
         ]
+
+        # remove file that are directories (legacy of s3contentmngr)
+        objects = [object for object in objects if object["Key"] not in directory_set]
 
         # Lock the bucket
         with transaction.atomic():
