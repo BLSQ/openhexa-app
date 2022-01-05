2d3113e4c44488eea3e5869da8b166551d8e5432
pvanliefland <pierre.vanliefland@gmail.com>
2022-01-05 12:58:53 +0100

    AC-21 Create AccessMod plugin in OpenHexa
    Plugin setup

---
+++ hexa/plugins/connector_accessmod/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ConnectorAccessmodConfig(AppConfig):
+    name = "hexa.plugins.connector_accessmod"
+    label = "connector_accessmod"
+
+    verbose_name = "AccessMod Connector"
