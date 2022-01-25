e5fd392410bfa75d50b81db0390639bfc12505e0
pvanliefland <pierre.vanliefland@gmail.com>
2022-01-25 11:50:10 +0100

    Index troubleshooting

--- hexa/catalog/management/commands/index_troubleshoot.py
+++ hexa/catalog/management/commands/index_troubleshoot.py
@@ -10,9 +10,11 @@ logger = getLogger(__name__)
 class Command(BaseCommand):
     def handle(self, *args, **options):
         indexes = Index.objects.all().iterator(chunk_size=50)
+        orphan_count = 0
 
         for index in indexes:
             if index.object is None:
+                orphan_count += 1
                 created_at = index.created_at.strftime("%d/%m/%y")
                 updated_at = index.updated_at.strftime("%d/%m/%y")
                 self.stdout.write(
@@ -20,3 +22,10 @@ class Command(BaseCommand):
                         f"Orphan: Index {index.id} ({created_at}, {updated_at}) for CT {index.content_type.name}"
                     )
                 )
+
+        if orphan_count == 0:
+            self.stdout.write(self.style.SUCCESS("No orphaned index found"))
+        else:
+            self.stdout.write(
+                self.style.WARNING(f"Found {orphan_count} orphaned indexes")
+            )
