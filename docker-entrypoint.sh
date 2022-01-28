5cfc74efd5d3bc213c726ffea96fe3515269b9aa
Quentin Gérôme <geromequentin@gmail.com>
2022-01-28 16:27:36 +0100

    Rename "manage.py index" command to "manage.py datasource_index"

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -67,7 +67,7 @@ case "$command" in
     python manage.py migrate
     python manage.py createsuperuser --no-input || true
     python manage.py loaddata demo.json
-    python manage.py index
+    python manage.py datasource_index
   else
     echo "The \"fixtures\" command can only be executed in dev mode"
   fi
