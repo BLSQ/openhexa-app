e80f63bfea29411f1aea529194ef058b2d0942bb
pvanliefland <pierre.vanliefland@gmail.com>
2022-03-24 14:11:26 +0100

    Extra test

--- hexa/catalog/tests/test_models.py
+++ hexa/catalog/tests/test_models.py
@@ -1,4 +1,5 @@
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
 
 from hexa.core.test import TestCase
 from hexa.user_management.models import Membership, Team, User
@@ -9,9 +10,13 @@ from ..models import Index, IndexPermission
 class PermissionTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.USER_REGULAR = User.objects.create_user(
+        cls.USER_REGULAR_1 = User.objects.create_user(
             "jim@bluesquarehub.com",
-            "regular",
+            "jimmyjim",
+        )
+        cls.USER_REGULAR_2 = User.objects.create_user(
+            "jane@bluesquarehub.com",
+            "janyjane",
         )
         cls.USER_SUPER = User.objects.create_user(
             "mary@bluesquarehub.com",
@@ -20,15 +25,17 @@ class PermissionTest(TestCase):
         )
         cls.TEAM1 = Team.objects.create(name="Test Team1")
         cls.TEAM2 = Team.objects.create(name="Test Team2")
-        Membership.objects.create(team=cls.TEAM1, user=cls.USER_REGULAR)
-        Membership.objects.create(team=cls.TEAM2, user=cls.USER_REGULAR)
+        Membership.objects.create(team=cls.TEAM1, user=cls.USER_REGULAR_1)
+        Membership.objects.create(team=cls.TEAM2, user=cls.USER_REGULAR_1)
 
         # index over users. dont mind the object_id, it should not have any impact
-        ct = ContentType.objects.filter(
+        cls.CT = ContentType.objects.filter(
             app_label="user_management", model="user"
         ).first()
-        cls.I1 = Index.objects.create(content_type=ct, object_id=cls.USER_SUPER.id)
-        cls.I2 = Index.objects.create(content_type=ct, object_id=cls.USER_REGULAR.id)
+        cls.I1 = Index.objects.create(content_type=cls.CT, object_id=cls.USER_SUPER.id)
+        cls.I2 = Index.objects.create(
+            content_type=cls.CT, object_id=cls.USER_REGULAR_1.id
+        )
         IndexPermission.objects.create(index=cls.I1, team=cls.TEAM1)
         IndexPermission.objects.create(index=cls.I1, team=cls.TEAM2)
 
@@ -39,7 +46,7 @@ class PermissionTest(TestCase):
         """
         self.assertEqual(
             list(
-                Index.objects.filter_for_user(self.USER_REGULAR)
+                Index.objects.filter_for_user(self.USER_REGULAR_1)
                 .order_by("object_id")
                 .values("object_id")
             ),
@@ -53,9 +60,18 @@ class PermissionTest(TestCase):
             ),
             sorted(
                 [
-                    {"object_id": self.USER_REGULAR.id},
+                    {"object_id": self.USER_REGULAR_1.id},
                     {"object_id": self.USER_SUPER.id},
                 ],
                 key=lambda o: o["object_id"],
             ),
         )
+
+    def test_filter_for_user(self):
+        index = Index.objects.create(content_type=self.CT, object_id=self.USER_SUPER.id)
+        IndexPermission.objects.create(index=index, team=self.TEAM1)
+        self.assertEqual(
+            index, Index.objects.filter_for_user(self.USER_REGULAR_1).get(id=index.id)
+        )
+        with self.assertRaises(ObjectDoesNotExist):
+            Index.objects.filter_for_user(self.USER_REGULAR_2).get(id=index.id)
