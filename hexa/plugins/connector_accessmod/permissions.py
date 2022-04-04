61397b4d38c6c5c1a976bbeecd0d7029289b93f5
pvanliefland <pierre.vanliefland@gmail.com>
2022-04-04 09:55:05 +0200

    Cleanup

--- hexa/plugins/connector_accessmod/permissions.py
+++ hexa/plugins/connector_accessmod/permissions.py
@@ -8,10 +8,10 @@ from hexa.user_management import models as user_management_models
 class TeamPermissions(enum.Enum):
     @staticmethod
     def add_user_to_team(
-        user: user_management_models.User, team: user_management_models.Team
+        principal: user_management_models.User, team: user_management_models.Team
     ):
         """Only team admins can add users to a team"""
 
-        return user.membership_set.filter(
+        return principal.membership_set.filter(
             team=team, role=user_management_models.MembershipRole.ADMIN
         ).exists()
