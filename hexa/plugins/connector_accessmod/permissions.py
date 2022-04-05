d3cfc401fa4ee868d6b2a330de90d2a8d9e9725d
pvanliefland <pierre.vanliefland@gmail.com>
2022-04-05 11:56:43 +0200

    WIP

--- hexa/plugins/connector_accessmod/permissions.py
+++ hexa/plugins/connector_accessmod/permissions.py
@@ -7,8 +7,22 @@ from hexa.user_management import models as user_management_models
 
 
 class TeamPermissions(enum.Enum):
-    @staticmethod
+    def edit_team(
+        self, principal: user_management_models.User, team: user_management_models.Team
+    ):
+        """Only team admins can edit the team."""
+
+        return self._has_admin_membership(principal, team)
+
+    def delete_team(
+        self, principal: user_management_models.User, team: user_management_models.Team
+    ):
+        """Only team admins can delete a team."""
+
+        return self._has_admin_membership(principal, team)
+
     def add_user_to_team(
+        self,
         principal: user_management_models.User,
         user_and_team: typing.Sequence[
             user_management_models.User, user_management_models.Team
@@ -18,15 +32,10 @@ class TeamPermissions(enum.Enum):
 
         user, team = user_and_team
 
-        return (
-            user.is_active
-            and principal.membership_set.filter(
-                team=team, role=user_management_models.MembershipRole.ADMIN
-            ).exists()
-        )
+        return user.is_active and self._has_admin_membership(principal, team)
 
-    @staticmethod
     def remove_user_from_team(
+        self,
         principal: user_management_models.User,
         user_and_team: typing.Sequence[
             user_management_models.User, user_management_models.Team
@@ -35,9 +44,11 @@ class TeamPermissions(enum.Enum):
         """Only team admins can remove users from a team, and the admin cannot remove himself from the team."""
         user, team = user_and_team
 
-        return (
-            not user == principal
-            and principal.membership_set.filter(
-                team=team, role=user_management_models.MembershipRole.ADMIN
-            ).exists()
-        )
+        return not user == principal and self._has_admin_membership(principal, team)
+
+    def _has_admin_membership(
+        self, principal: user_management_models.User, team: user_management_models.Team
+    ):
+        return principal.membership_set.filter(
+            team=team, role=user_management_models.MembershipRole.ADMIN
+        ).exists()
