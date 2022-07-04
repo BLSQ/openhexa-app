b94fee3a26e219fbb5c32d577d857591c5a364ab
wagzi <issouf.waga@gmail.com>
2022-07-04 14:20:08 +0200

    S3 links with '//'

--- hexa/plugins/connector_s3/urls.py
+++ hexa/plugins/connector_s3/urls.py
@@ -7,7 +7,7 @@ app_name = "connector_s3"
 urlpatterns = [
     path("<uuid:datasource_id>/", views.datasource_detail, name="datasource_detail"),
     path(
-        "<uuid:bucket_id>/object/<path:path>/",
+        "<uuid:bucket_id>/object/<path:path>",
         views.object_detail,
         name="object_detail",
     ),
