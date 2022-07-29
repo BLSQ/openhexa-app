ac47e3e9090d1491f9a76fd5ef8605ee24da9bce
Quentin Gérôme <geromequentin@gmail.com>
2022-07-29 09:45:44 +0000

    Add tags resolver

--- hexa/tags/schema.py
+++ hexa/tags/schema.py
@@ -1,12 +1,12 @@
 import pathlib
 
-from ariadne import MutationType, QueryType, load_schema_from_path
+from ariadne import ObjectType, load_schema_from_path
 
 tags_type_defs = load_schema_from_path(
     f"{pathlib.Path(__file__).parent.resolve()}/graphql/schema.graphql"
 )
-tags_query = QueryType()
-tags_mutations = MutationType()
 
 
-tags_bindables = []
+tag_object = ObjectType("Tag")
+
+tags_bindables = [tag_object]
