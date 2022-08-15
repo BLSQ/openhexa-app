e56806ababae967af925379f6d864746ea439e58
pvanliefland <pierre.vanliefland@gmail.com>
2022-08-15 16:11:15 +0200

    Simplified header handling in proxy

--- src/pages/api/proxy.ts
+++ src/pages/api/proxy.ts
@@ -13,26 +13,13 @@ export default async function proxy(req: NextRequest) {
   const url = new URL(req.url);
   const fallbackURL = `${process.env.FALLBACK_URL}${url.pathname}${url.search}`;
 
-  const cookie = ["sessionid", "csrftoken"]
-    .map((key) => {
-      const value = req.cookies.get(key);
-      if (value) {
-        return `${key}=${value}`;
-      }
-      return null;
-    })
-    .filter((c) => c !== null)
-    .join(";");
-
   const response = await fetch(fallbackURL, {
-    headers: { cookie },
     credentials: "include",
+    headers: req.headers,
   });
 
   return new Response(response.body, {
     status: response.status,
-    headers: {
-      "Content-Type": response.headers.get("Content-Type") || "text/html",
-    },
+    headers: response.headers,
   });
 }
