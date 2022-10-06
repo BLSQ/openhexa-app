2f896d00c18e49ccb4bd9b33693337c4ad2773fd
Quentin Gérôme <geromequentin@gmail.com>
2022-10-06 15:45:38 +0200

    chore(i18n): Fix next warnings

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   i18n: {
-    defaultNS: "messages",
     defaultLocale: "en",
-    fallbackLng: "en",
     locales: ["en", "fr"],
-    pluralSeparator: "___",
-    // This option will reload your translations whenever serverSideTranslations is called
-    reloadOnPrerender: true,
+  },
+  defaultNS: "messages",
+  fallbackLng: "en",
+  pluralSeparator: "___",
+  // This option will reload your translations whenever serverSideTranslations is called
+  reloadOnPrerender: true,
 
-    // allow an empty value to count as invalid (by default is true)
-    returnEmptyString: false,
+  // allow an empty value to count as invalid (by default is true)
+  returnEmptyString: false,
 
-    react: {
-      // Which nodes are allowed to be kept in translations during defaultValue generation of <Trans>
-      transKeepBasicHtmlNodesFor: ["b", "br", "strong", "i", "p"],
-    },
+  react: {
+    // Which nodes are allowed to be kept in translations during defaultValue generation of <Trans>
+    transKeepBasicHtmlNodesFor: ["b", "br", "strong", "i", "p"],
   },
 };
