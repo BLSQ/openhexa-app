dd95aaec7fd141bd2029f556d0b756e633932ad9
Quentin Gérôme <geromequentin@gmail.com>
2022-10-12 10:29:44 +0000

    fix(Sentry): Provide a traces_sampler function The goal is to ignore / ready requests

--- config/settings.py
+++ config/settings.py
@@ -281,13 +281,34 @@ if SENTRY_DSN:
     # with the IP instead of the host
     ignore_logger("django.security.DisallowedHost")
 
+    # Sampling rate
+    traces_sample_rate = float(os.environ.get("SENTRY_TRACES_SAMPLE_RATE", "1.0"))
+
+    # Exclude /ready from sentry
+    def sentry_tracer_sampler(sampling_context):
+        transaction_context = sampling_context.get("transaction_context")
+        if transaction_context is None:
+            return 0
+
+        op = transaction_context.get("op")
+
+        if op == "http.server":
+            path = sampling_context.get("wsgi_environ", {}).get("PATH_INFO")
+            # Monitoring endpoints
+            if path.startswith("/ready"):
+                return 0
+
+        # Default sample rate for everything else
+        return traces_sample_rate
+
     # inject sentry into logging config. set level to ERROR, we don't really want the rest?
     sentry_logging = LoggingIntegration(level=logging.ERROR, event_level=logging.ERROR)
 
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         integrations=[DjangoIntegration(), sentry_logging],
-        traces_sample_rate=float(os.environ.get("SENTRY_TRACES_SAMPLE_RATE", "1")),
+        traces_sample_rate=traces_sample_rate,
+        traces_sampler=sentry_tracer_sampler,
         send_default_pii=True,
         environment=os.environ.get("SENTRY_ENVIRONMENT"),
     )
