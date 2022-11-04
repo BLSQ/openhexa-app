5944f0a1352728e3228213f3d66bb86e866685e9
Quentin Gérôme <geromequentin@gmail.com>
2022-11-04 11:27:02 +0100

    chore(Page): Pass className to child

--- src/core/components/Layout/PageContent.tsx
+++ src/core/components/Layout/PageContent.tsx
@@ -5,7 +5,10 @@ import { LayoutClasses } from "./styles";
 export function PageContent(props: HTMLAttributes<HTMLDivElement>) {
   const { children, className, ...delegated } = props;
   return (
-    <main className={clsx(LayoutClasses.container, "pb-6")} {...delegated}>
+    <main
+      className={clsx(LayoutClasses.container, "pb-6", className)}
+      {...delegated}
+    >
       {children}
     </main>
   );
