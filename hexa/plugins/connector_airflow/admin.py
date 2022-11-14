275609c00396515a475d556931397eee926aa3ba
ekatsah <github@hastake.com>
2022-11-14 09:39:33 +0100

    Action for airflow cluster sync in openhexa admin (#359)

--- hexa/plugins/connector_airflow/admin.py
+++ hexa/plugins/connector_airflow/admin.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.admin import display
+from django.contrib.contenttypes.models import ContentType
 from django.utils.html import format_html
+
+from hexa.pipelines.queue import environment_sync_queue
 
 from .models import (
     DAG,
@@ -23,9 +27,34 @@ class DAGAuthorizedDatasourceInline(admin.TabularInline):
     verbose_name = "Authorized Datasource"
 
 
+@admin.action(description="Sync selected Airflow cluster(s)")
+def SyncCluster(modeladmin: admin.ModelAdmin, request, queryset):
+    for cluster in list(queryset):
+        if settings.EXTERNAL_ASYNC_REFRESH:
+            environment_sync_queue.enqueue(
+                "environment_sync",
+                {
+                    "contenttype_id": str(
+                        ContentType.objects.get_for_model(cluster).id
+                    ),
+                    "object_id": str(cluster.id),
+                },
+            )
+            modeladmin.message_user(request, "The environment will soon be synced")
+        else:
+            try:
+                sync_result = cluster.sync()
+                modeladmin.message_user(request, sync_result)
+            except Exception:
+                modeladmin.message_user(
+                    request, f"Sync failed for environment {cluster.id}"
+                )
+
+
 @admin.register(Cluster)
 class ClusterAdmin(admin.ModelAdmin):
     list_display = ("name", "get_url", "last_synced_at", "auto_sync")
+    actions = [SyncCluster]
 
     @display(ordering="url", description="Url")
     def get_url(self, obj):
