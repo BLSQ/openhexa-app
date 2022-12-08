ac99af04850793275662c1ca701f2e3c55e4925e
Cheikh Gueye Wane <cheikhgueye.wane@gmail.com>
2022-12-08 08:56:38 +0000

    fix:load favicon file (#151)
    Co-authored-by: cheikhgwane <cwane@bluesquarehub.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ class Document extends BaseDocument {
   render() {
     return (
       <Html className="h-full bg-gray-100">
-        <Head />
+        <Head>
+          <link rel="icon" href="/static/img/favicon.png" />
+        </Head>
         <body className="h-full">
           <Main />
           <NextScript />
