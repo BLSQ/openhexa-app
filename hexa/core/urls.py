213de0146e1121cf10141f54a7d73e5fb8bcb343
cheikhgwane <cwane@bluesquarehub.com>
2022-12-15 12:07:50 +0000

    remove unused code for dashboard

--- hexa/core/urls.py
+++ hexa/core/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
+from hexa.core import views_utils
+
 from . import views
 
 app_name = "core"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views_utils.redirect_to_new_frontend, name="index"),
     path("ready", views.ready, name="ready"),
-    path("dashboard/", views.dashboard, name="dashboard"),
+    path("dashboard/", views_utils.redirect_to_new_frontend, name="dashboard"),
     path("test_logger", views.test_logger, name="test_logger"),
 ]
