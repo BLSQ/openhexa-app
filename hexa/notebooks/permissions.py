58436756fa76f9660b90108a3e86135a1d80bd3b
pvanliefland <pierre.vanliefland@gmail.com>
2023-02-20 16:32:25 +0100

    Notebooks in workspaces (cleanup)

--- hexa/notebooks/permissions.py
+++
@@ -1,7 +0,0 @@
-from hexa.user_management.models import User
-
-
-def create_server(principal: User):
-    """Authenticated users can create notebook servers"""
-
-    return principal.is_authenticated
--- hexa/notebooks/schema.py
+++ hexa/notebooks/schema.py
@@ -5,6 +5,8 @@ from ariadne import MutationType, QueryType, load_schema_from_path
 from django.conf import settings
 from django.http import HttpRequest
 
+from hexa.workspaces.models import Workspace
+
 notebooks_type_defs = load_schema_from_path(
     f"{pathlib.Path(__file__).parent.resolve()}/graphql/schema.graphql"
 )
@@ -24,11 +26,20 @@ def resolve_launch_notebook_server(_, info, input, **kwargs):
     Jupyterhub."""
 
     request: HttpRequest = info.context["request"]
+    workspace_slug = input["workspaceSlug"]
 
-    if not request.user.has_perm("notebooks.create_server"):  # TODO: check workspace
+    try:
+        workspace = Workspace.objects.filter_for_user(request.user).get(
+            slug=workspace_slug
+        )
+    except Workspace.DoesNotExist:
+        return {"success": False, "server": None, "errors": ["NOT_FOUND"]}
+
+    if not request.user.has_perm(
+        "workspaces.launch_notebooks", workspace
+    ):  # TODO: check workspace
         return {"success": False, "server": None, "errors": ["PERMISSION_DENIED"]}
 
-    workspace_slug = input["workspaceSlug"]
     headers = {
         "Authorization": f"token {settings.NOTEBOOKS_APP_HUB_AUTH_TOKEN}",
     }
@@ -37,15 +48,12 @@ def resolve_launch_notebook_server(_, info, input, **kwargs):
     user_response = requests.get(
         f"{settings.NOTEBOOKS_API_URL}/users/{request.user.email}", headers=headers
     )
-    if user_response.status_code != 200:
+    if user_response.status_code == 404:
         user_response = requests.post(
             f"{settings.NOTEBOOKS_API_URL}/users/{request.user.email}", headers=headers
         )
         if user_response.status_code != 201:
             return {"success": False, "server": None, "errors": ["UNKNOWN_ERROR"]}
-
-    # TODO: check  # https://discourse.jupyter.org/t/setting-environment-variables-dynamically/9982
-    # TODO: and https://github.com/jupyterhub/zero-to-jupyterhub-k8s/issues/2087
 
     if workspace_slug not in user_response.json()["servers"]:
         server_response = requests.post(
