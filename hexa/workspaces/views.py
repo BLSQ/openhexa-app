401a68873fc5842435d9463ef1facb23566f6397
cheikhgwane <cwane@bluesquarehub.com>
2023-03-06 14:35:59 +0000

    feat(Workspaces): inject database credentials into workspace notebooks

--- hexa/workspaces/views.py
+++ hexa/workspaces/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.views.decorators.http import require_POST
 
+from hexa.databases.api import get_db_server_credentials
 from hexa.workspaces.models import Workspace
 
 
@@ -34,6 +35,18 @@ def credentials(request: HttpRequest, workspace_slug: str) -> HttpResponse:
         env.update(connection.env_variables)
 
     # TODO: Database / Filesystem credentials
+
+    # Database credentials
+    db_credentials = get_db_server_credentials()
+    env.update(
+        {
+            "WORKSPACE_DATABASE_HOST": db_credentials["host"],
+            "WORKSPACE_DATABASE_PORT": db_credentials["port"],
+            "WORKSPACE_DATABASE_USERNAME": workspace.db_name,
+            "WORKSPACE_DATABASE_PASSWORD": workspace.db_password,
+            "WORKSPACE_DATABASE_URL": f"postgresql://{workspace.db_name}:{workspace.db_password}@{db_credentials['host']}:{db_credentials['port']}/{workspace.db_name}",
+        }
+    )
 
     return JsonResponse(
         {"env": env},
