58ad45140bc69df18391b28fe4321b2895ca04bf
Quentin Gérôme <quentin@letsbuildit.dev>
2023-05-11 17:01:15 +0200

    fix(Sidebar): in compact mode it should not be possible to focus the tooltip

--- src/workspaces/layouts/WorkspaceLayout/Sidebar.tsx
+++ src/workspaces/layouts/WorkspaceLayout/Sidebar.tsx
@@ -67,7 +67,7 @@ const NavItem = (props: {
       ></div>
       <Icon className={clsx(compact ? "h-7 w-7" : "ml-1 h-5 w-5")} />
       {compact ? (
-        <div className="absolute inset-y-0 left-full ml-1.5 flex h-full items-center text-xs opacity-0 transition-opacity group-hover:opacity-100">
+        <div className="absolute inset-y-0 left-full ml-1.5 hidden h-full items-center text-xs opacity-0 transition-opacity group-hover:flex group-hover:opacity-100">
           <Badge className="bg-gray-800 ">{label}</Badge>
         </div>
       ) : (
