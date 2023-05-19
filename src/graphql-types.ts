c53617f84ba06146bb14ba033694c3f2a7a82c0d
Cheikh Gueye Wane <cwane@bluesquarehub.com>
2023-05-19 14:48:31 +0000

    Feat/hexa 345 usage sections simplification (#292)
    * feat(Connections): usage section simplification
    * feat(Database): display env variables and update usage section

--- src/graphql-types.ts
+++ src/graphql-types.ts
@@ -956,6 +956,7 @@ export type Dhis2Instance = {
 
 export type Database = {
   __typename?: 'Database';
+  credentials?: Maybe<DatabaseCredentials>;
   table?: Maybe<DatabaseTable>;
   tables: DatabaseTablePage;
 };
@@ -969,6 +970,16 @@ export type DatabaseTableArgs = {
 export type DatabaseTablesArgs = {
   page?: InputMaybe<Scalars['Int']>;
   perPage?: InputMaybe<Scalars['Int']>;
+};
+
+export type DatabaseCredentials = {
+  __typename?: 'DatabaseCredentials';
+  dbName: Scalars['String'];
+  host: Scalars['String'];
+  password: Scalars['String'];
+  port: Scalars['Int'];
+  url: Scalars['String'];
+  username: Scalars['String'];
 };
 
 export type DatabaseTable = {
