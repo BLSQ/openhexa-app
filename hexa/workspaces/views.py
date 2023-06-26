aaa83f8da9a2225827641a4161ab69dce8fdba3f
cheikhgwane <cwane@bluesquarehub.com>
2023-06-26 14:49:57 +0000

    fix(Workspaces): use proxy ENV as WORKSPACE_DATABASE_HOST

--- hexa/workspaces/views.py
+++ hexa/workspaces/views.py
@@ -88,7 +88,7 @@ def credentials(request: HttpRequest, workspace_slug: str = None) -> HttpRespons
     env.update(
         {
             "WORKSPACE_DATABASE_DB_NAME": workspace.db_name,
-            "WORKSPACE_DATABASE_HOST": db_credentials["host"],
+            "WORKSPACE_DATABASE_HOST": workspace.db_host,
             "WORKSPACE_DATABASE_PORT": db_credentials["port"],
             "WORKSPACE_DATABASE_USERNAME": workspace.db_name,
             "WORKSPACE_DATABASE_PASSWORD": workspace.db_password,
