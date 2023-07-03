98ab6354815714d4ef351e1ebfee9fafc77ddaa3
Quentin Gérôme <quentin@letsbuildit.dev>
2023-07-03 15:31:34 +0200

    chore(Backoff): Implement a backoff mechanism to retrieve the run status (#361)

--- src/pipelines/hooks/usePipelineRunPoller/usePipelineRunPoller.ts
+++ src/pipelines/hooks/usePipelineRunPoller/usePipelineRunPoller.ts
@@ -1,48 +1,43 @@
 import { gql, useLazyQuery } from "@apollo/client";
+import Backoff from "core/helpers/backoff";
 import { PipelineRun, PipelineRunStatus } from "graphql-types";
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { PipelineRunPollerQuery } from "./usePipelineRunPoller.generated";
 
-export function randomInterval(interval: number, factor: number) {
-  const variable = Math.floor(Math.random() * factor * interval);
-  return interval + variable * (Math.random() > 0.5 ? 1 : -1);
-}
-
-export default function usePipelineRunPoller(
-  runId: PipelineRun["id"] | null,
-  interval: number = 1000
-) {
-  const [_, { data, stopPolling, startPolling }] =
-    useLazyQuery<PipelineRunPollerQuery>(
-      gql`
-        query PipelineRunPoller($runId: UUID!) {
-          run: pipelineRun(id: $runId) {
-            id
-            status
-            duration
-            progress
-          }
+export default function usePipelineRunPoller(runId: PipelineRun["id"] | null) {
+  const backoffRef = useRef(
+    new Backoff({ min: 200, max: 10000, factor: 2, jitter: 0.2 })
+  );
+  const [fetch] = useLazyQuery<PipelineRunPollerQuery>(
+    gql`
+      query PipelineRunPoller($runId: UUID!) {
+        run: pipelineRun(id: $runId) {
+          id
+          status
+          duration
+          progress
         }
-      `,
-      { variables: { runId } }
-    );
+      }
+    `,
+    { variables: { runId } }
+  );
 
-  useEffect(() => {
-    if (!runId) {
-      stopPolling();
-    } else if (!data) {
-      startPolling(randomInterval(interval, 0.4));
-    } else if (
-      data.run &&
+  const retry = useCallback(async () => {
+    const { data } = await fetch({ fetchPolicy: "network-only" });
+    if (
+      data?.run &&
       [PipelineRunStatus.Queued, PipelineRunStatus.Running].includes(
         data.run.status
       )
     ) {
-      startPolling(randomInterval(interval, 0.4));
-    } else {
-      stopPolling();
+      setTimeout(() => retry(), backoffRef.current.duration());
     }
+  }, [fetch]);
 
-    return () => stopPolling();
-  }, [data, startPolling, stopPolling, runId, interval]);
+  useEffect(() => {
+    backoffRef.current.reset();
+    if (runId) {
+      retry();
+    }
+  }, [runId, retry]);
 }
