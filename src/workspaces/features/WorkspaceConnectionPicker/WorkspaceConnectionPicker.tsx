841e08b1ec4ba224ce66d8f079292347a60e9d9e
Cheikh Gueye Wane <cwane@bluesquarehub.com>
2023-12-04 12:11:08 +0000

    fix(ConnectionPicker): use id for connection identifier (#493)
    * fix(ConnectionPicker): use id for connection identifier
    
    * Update src/workspaces/features/WorkspaceConnectionPicker/WorkspaceConnectionPicker.tsx
    
    Co-authored-by: Quentin Gérôme <quentin@letsbuildit.dev>
    
    ---------
    
    Co-authored-by: Quentin Gérôme <quentin@letsbuildit.dev>

--- src/workspaces/features/WorkspaceConnectionPicker/WorkspaceConnectionPicker.tsx
+++ src/workspaces/features/WorkspaceConnectionPicker/WorkspaceConnectionPicker.tsx
@@ -85,7 +85,7 @@ const WorkspaceConnectionPicker = (props: WorkspaceConnectionPickerProps) => {
       loading={loading}
       withPortal={withPortal}
       displayValue={displayValue}
-      by="name"
+      by="slug"
       onInputChange={useCallback(
         (event: any) => setQuery(event.target.value),
         [],
