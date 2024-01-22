0a6a35bd804e28f4a965724c60f44c55bf79cb9f
Stéphan Mestach <smestach@bluesquare.org>
2024-01-22 10:04:01 +0100

    fix: remove unused variables

--- hexa/files/api.py
+++ hexa/files/api.py
@@ -1,7 +1,8 @@
-from django.conf import settings
-
+from .basefs import NotFound
 from .gcp import GCPClient
 from .s3 import S3Client
+
+from django.conf import settings
 
 def get_storage(mode=settings.WORKSPACE_STORAGE_ENGINE):
     if mode == "gcp":
