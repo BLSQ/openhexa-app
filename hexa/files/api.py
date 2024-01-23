0ef6dc764d641c93795d274a24928e1ad8064312
Stéphan Mestach <smestach@bluesquare.org>
2024-01-23 14:15:41 +0100

    fix: linter but don't loose NotFound

--- hexa/files/api.py
+++ hexa/files/api.py
@@ -1,8 +1,9 @@
+from django.conf import settings
+
 from .basefs import NotFound
 from .gcp import GCPClient
 from .s3 import S3Client
 
-from django.conf import settings
 
 def get_storage(mode=settings.WORKSPACE_STORAGE_ENGINE):
     if mode == "gcp":
@@ -10,3 +11,6 @@ def get_storage(mode=settings.WORKSPACE_STORAGE_ENGINE):
     if mode == "s3":
         return S3Client()
     raise Exception(f"unsupported filesystem {mode}")
+
+
+NotFound = NotFound
