d370863b762e0fd0a9ba23ac2d9e33ce0314e688
Quentin Gérôme <geromequentin@gmail.com>
2024-02-08 10:54:44 +0100

    chore(Debt): Start to remove the comments app

--- hexa/comments/__init__.py
+++
@@ -1,4 +0,0 @@
-def get_model():
-    from .models import Comment
-
-    return Comment
--- hexa/comments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "comments"
-
-urlpatterns = [
-    path("add/", views.comments, name="post_comment"),
-]
