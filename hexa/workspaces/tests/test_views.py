41329bc85eb67b943d3612236e7f51baa4fbaf48
Cheikh Gueye Wane <cwane@bluesquarehub.com>
2024-06-07 12:08:25 +0000

    chore(Connections): stop injecting connections creds in env var

--- hexa/workspaces/tests/test_views.py
+++ hexa/workspaces/tests/test_views.py
@@ -9,8 +9,6 @@ from hexa.files.tests.mocks.mockgcp import mock_gcp_storage
 from hexa.pipelines.models import Pipeline, PipelineRunTrigger
 from hexa.user_management.models import Feature, FeatureFlag, User
 from hexa.workspaces.models import (
-    Connection,
-    ConnectionType,
     Workspace,
     WorkspaceMembership,
     WorkspaceMembershipRole,
@@ -54,92 +52,6 @@ class ViewsTest(TestCase):
             user=cls.USER_REBECCA,
             workspace=cls.WORKSPACE,
             role=WorkspaceMembershipRole.VIEWER,
-        )
-
-        cls.WORKSPACE_POSTGRESQL_CONNECTION = Connection.objects.create_if_has_perm(
-            cls.USER_JULIA,
-            cls.WORKSPACE,
-            name="DB",
-            description="Connection's description",
-            connection_type=ConnectionType.POSTGRESQL,
-        )
-
-        cls.WORKSPACE_POSTGRESQL_CONNECTION.set_fields(
-            cls.USER_JULIA,
-            [
-                {"code": "host", "value": "127.0.0.1"},
-                {"code": "port", "value": "5432"},
-                {"code": "username", "value": "hexa-app"},
-                {"code": "password", "secret": True, "value": "hexa-app"},
-                {"code": "db_name", "secret": False, "value": "hexa-app"},
-            ],
-        )
-
-        cls.WORKSPACE_S3_CONNECTION = Connection.objects.create_if_has_perm(
-            cls.USER_JULIA,
-            cls.WORKSPACE,
-            name="S3",
-            description="S3 connection",
-            connection_type=ConnectionType.S3,
-        )
-
-        cls.WORKSPACE_S3_CONNECTION.set_fields(
-            cls.USER_JULIA,
-            [
-                {"code": "bucket_name", "value": "bucket_name"},
-                {"code": "access_key_id", "value": "access_key"},
-                {"code": "secret_access_key", "value": "hexa-app"},
-            ],
-        )
-
-        cls.WORKSPACE_GCS_CONNECTION = Connection.objects.create_if_has_perm(
-            cls.USER_JULIA,
-            cls.WORKSPACE,
-            name="GCS",
-            description="GCS connection",
-            connection_type=ConnectionType.GCS,
-        )
-
-        cls.WORKSPACE_GCS_CONNECTION.set_fields(
-            cls.USER_JULIA,
-            [
-                {"code": "bucket_name", "value": "bucket_name"},
-                {"code": "service_account_key", "value": "key"},
-            ],
-        )
-
-        cls.WORKSPACE_IASO_CONNECTION = Connection.objects.create_if_has_perm(
-            cls.USER_JULIA,
-            cls.WORKSPACE,
-            name="IASO",
-            description="IASO connection",
-            connection_type=ConnectionType.IASO,
-        )
-
-        cls.WORKSPACE_IASO_CONNECTION.set_fields(
-            cls.USER_JULIA,
-            [
-                {"code": "url", "value": "url"},
-                {"code": "username", "value": "user"},
-                {"code": "password", "secret": True, "value": "admin"},
-            ],
-        )
-
-        cls.WORKSPACE_DHIS2_CONNECTION = Connection.objects.create_if_has_perm(
-            cls.USER_JULIA,
-            cls.WORKSPACE,
-            name="DHIS2",
-            description="DHIS2 connection",
-            connection_type=ConnectionType.DHIS2,
-        )
-
-        cls.WORKSPACE_DHIS2_CONNECTION.set_fields(
-            cls.USER_JULIA,
-            [
-                {"code": "url", "value": "url"},
-                {"code": "username", "value": "user"},
-                {"code": "password", "secret": True, "value": "admin"},
-            ],
         )
 
         cls.PIPELINE = Pipeline.objects.create(
@@ -205,13 +117,6 @@ class ViewsTest(TestCase):
         self.assertEqual(
             response_data["env"],
             {
-                "DB_HOST": "127.0.0.1",
-                "DB_PORT": "5432",
-                "DB_USERNAME": "hexa-app",
-                "DB_PASSWORD": "hexa-app",
-                "DB_DATABASE": "hexa-app",  # Kept for backward-compat
-                "DB_DB_NAME": "hexa-app",
-                "DB_URL": "postgresql://hexa-app:hexa-app@127.0.0.1:5432/hexa-app",
                 "WORKSPACE_BUCKET_NAME": self.WORKSPACE.bucket_name,
                 "WORKSPACE_DATABASE_DB_NAME": self.WORKSPACE.db_name,
                 "WORKSPACE_DATABASE_HOST": self.WORKSPACE.db_host,
@@ -225,43 +130,6 @@ class ViewsTest(TestCase):
                 "HEXA_TOKEN": Signer().sign_object(
                     self.WORKSPACE_MEMBERSHIP_JULIA.access_token
                 ),
-                "GCS_BUCKET_NAME": self.WORKSPACE_GCS_CONNECTION.fields.get(
-                    code="bucket_name"
-                ).value,
-                "GCS_SERVICE_ACCOUNT_KEY": self.WORKSPACE_GCS_CONNECTION.fields.get(
-                    code="service_account_key"
-                ).value,
-                "IASO_API_URL": self.WORKSPACE_IASO_CONNECTION.fields.get(
-                    code="url"
-                ).value,
-                "IASO_URL": self.WORKSPACE_IASO_CONNECTION.fields.get(code="url").value,
-                "IASO_USERNAME": self.WORKSPACE_IASO_CONNECTION.fields.get(
-                    code="username"
-                ).value,
-                "IASO_PASSWORD": self.WORKSPACE_IASO_CONNECTION.fields.get(
-                    code="password"
-                ).value,
-                "S3_ACCESS_KEY_ID": self.WORKSPACE_S3_CONNECTION.fields.get(
-                    code="access_key_id"
-                ).value,
-                "S3_BUCKET_NAME": self.WORKSPACE_S3_CONNECTION.fields.get(
-                    code="bucket_name"
-                ).value,
-                "S3_SECRET_ACCESS_KEY": self.WORKSPACE_S3_CONNECTION.fields.get(
-                    code="secret_access_key"
-                ).value,
-                "DHIS2_API_URL": self.WORKSPACE_DHIS2_CONNECTION.fields.get(
-                    code="url"
-                ).value,
-                "DHIS2_URL": self.WORKSPACE_DHIS2_CONNECTION.fields.get(
-                    code="url"
-                ).value,
-                "DHIS2_USERNAME": self.WORKSPACE_DHIS2_CONNECTION.fields.get(
-                    code="username"
-                ).value,
-                "DHIS2_PASSWORD": self.WORKSPACE_DHIS2_CONNECTION.fields.get(
-                    code="password"
-                ).value,
             },
         )
         self.assertEqual(
@@ -315,13 +183,6 @@ class ViewsTest(TestCase):
         self.assertEqual(
             response_data["env"],
             {
-                "DB_HOST": "127.0.0.1",
-                "DB_PORT": "5432",
-                "DB_USERNAME": "hexa-app",
-                "DB_PASSWORD": "hexa-app",
-                "DB_DATABASE": "hexa-app",  # Kept for backward-compat
-                "DB_DB_NAME": "hexa-app",
-                "DB_URL": "postgresql://hexa-app:hexa-app@127.0.0.1:5432/hexa-app",
                 "WORKSPACE_BUCKET_NAME": self.WORKSPACE.bucket_name,
                 "WORKSPACE_DATABASE_DB_NAME": self.WORKSPACE.db_name,
                 "WORKSPACE_DATABASE_HOST": self.WORKSPACE.db_host,
@@ -333,43 +194,6 @@ class ViewsTest(TestCase):
                 "WORKSPACE_STORAGE_ENGINE_GCP_ACCESS_TOKEN": "gcs-token",
                 "GCS_TOKEN": "gcs-token",
                 "HEXA_TOKEN": token,
-                "GCS_BUCKET_NAME": self.WORKSPACE_GCS_CONNECTION.fields.get(
-                    code="bucket_name"
-                ).value,
-                "GCS_SERVICE_ACCOUNT_KEY": self.WORKSPACE_GCS_CONNECTION.fields.get(
-                    code="service_account_key"
-                ).value,
-                "IASO_API_URL": self.WORKSPACE_IASO_CONNECTION.fields.get(
-                    code="url"
-                ).value,
-                "IASO_URL": self.WORKSPACE_IASO_CONNECTION.fields.get(code="url").value,
-                "IASO_USERNAME": self.WORKSPACE_IASO_CONNECTION.fields.get(
-                    code="username"
-                ).value,
-                "IASO_PASSWORD": self.WORKSPACE_IASO_CONNECTION.fields.get(
-                    code="password"
-                ).value,
-                "S3_ACCESS_KEY_ID": self.WORKSPACE_S3_CONNECTION.fields.get(
-                    code="access_key_id"
-                ).value,
-                "S3_BUCKET_NAME": self.WORKSPACE_S3_CONNECTION.fields.get(
-                    code="bucket_name"
-                ).value,
-                "S3_SECRET_ACCESS_KEY": self.WORKSPACE_S3_CONNECTION.fields.get(
-                    code="secret_access_key"
-                ).value,
-                "DHIS2_API_URL": self.WORKSPACE_DHIS2_CONNECTION.fields.get(
-                    code="url"
-                ).value,
-                "DHIS2_URL": self.WORKSPACE_DHIS2_CONNECTION.fields.get(
-                    code="url"
-                ).value,
-                "DHIS2_USERNAME": self.WORKSPACE_DHIS2_CONNECTION.fields.get(
-                    code="username"
-                ).value,
-                "DHIS2_PASSWORD": self.WORKSPACE_DHIS2_CONNECTION.fields.get(
-                    code="password"
-                ).value,
             },
         )
         self.assertEqual(
