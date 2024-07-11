6a35a8f04a4f85795d2d8db9208fde334ad97cc7
nazarfil <nfilipchuk@bluesquarehub.com>
2024-07-11 11:28:55 +0200

    fix: fixes api call parameters

--- hexa/datasets/schema/mutations.py
+++ hexa/datasets/schema/mutations.py
@@ -267,6 +267,12 @@ def resolve_create_version_file(_, info, **kwargs):
         return {"success": False, "errors": ["PERMISSION_DENIED"]}
 
 
+@mutations.field("createDatasetVersionFileSnapshot")
+def resolve_create_version_file_snapshot(_, info, **kwargs):
+    # TODO: implement flow to create snapshot via an API call
+    raise NotImplementedError
+
+
 @mutations.field("prepareVersionFileDownload")
 def resolve_version_file_download(_, info, **kwargs):
     request = info.context["request"]
