193d48df7c29a8f4c9ebf79591e69f103c962e51
Quentin Gérôme <quentin@letsbuildit.dev>
2024-07-11 10:00:11 +0200

    wip

--- hexa/core/analytics.py
+++ hexa/core/analytics.py
@@ -6,8 +6,9 @@ from ua_parser import user_agent_parser
 
 from hexa.user_management.models import AnonymousUser, User
 
-mixpanel_consumer = BufferedConsumer(max_size=1)
-mixpanel = Mixpanel(token=settings.MIXPANEL_TOKEN, consumer=mixpanel_consumer)
+mixpanel = None
+if settings.MIXPANEL_TOKEN:
+    mixpanel = Mixpanel(token=settings.MIXPANEL_TOKEN, consumer=BufferedConsumer())
 
 
 def track(
@@ -28,50 +29,52 @@ def track(
     user: User |
        User entity to track
     """
-    if settings.MIXPANEL_TOKEN:
-        # First check if we have a PipelineUser and get the associated user if it exits
-        # user will be None in case of pipeline triggered via schedule or webhook
-        if request:
-            parsed = user_agent_parser.Parse(request.headers["User-Agent"])
-            properties.update(
-                {
-                    "$browser": parsed["user_agent"]["family"],
-                    "$device": parsed["device"]["family"],
-                    "$os": parsed["os"]["family"],
-                    "ip": request.META["REMOTE_ADDR"],
-                }
-            )
+    if mixpanel is None:
+        return
+    people = user if user else getattr(request, "user", None)
+    can_track = (
+        people is None or isinstance(user, AnonymousUser) or people.analytics_enabled
+    )
+    if can_track is False:
+        return
 
-        can_track = (
-            user is None or isinstance(user, AnonymousUser) or user.analytics_enabled
+    if request:
+        # Add request related properties
+        parsed = user_agent_parser.Parse(request.headers["User-Agent"])
+        properties.update(
+            {
+                "$browser": parsed["user_agent"]["family"],
+                "$device": parsed["device"]["family"],
+                "$os": parsed["os"]["family"],
+                "ip": request.META["REMOTE_ADDR"],
+            }
         )
-        if can_track:
-            try:
-                mixpanel.track(
-                    distinct_id=str(user.id) if user else None,
-                    event_name=event,
-                    properties=properties,
-                )
-            except Exception as e:
-                print(e, flush=True)
-                capture_exception(e)
+    try:
+        mixpanel.track(
+            distinct_id=str(people.id) if people else None,
+            event_name=event,
+            properties=properties,
+        )
+    except Exception as e:
+        capture_exception(e)
 
 
 def set_user_properties(user: User):
-    if settings.MIXPANEL_TOKEN and user.analytics_enabled:
-        try:
-            mixpanel.people_set_once(
-                distinct_id=str(user.id),
-                properties={
-                    "$email": user.email,
-                    "$name": user.display_name,
-                    "is_staff": user.is_staff,
-                    "is_superuser": user.is_superuser,
-                    "email_domain": user.email.split("@")[1],
-                    "features_flag": [
-                        f.feature.code for f in user.featureflag_set.all()
-                    ],
-                },
-            )
-        except Exception as e:
-            capture_exception(e)
+    if mixpanel is None:
+        return
+    if user.analytics_enabled is False:
+        return
+
+    try:
+        mixpanel.people_set_once(
+            distinct_id=str(user.id),
+            properties={
+                "$email": user.email,
+                "$name": user.display_name,
+                "is_staff": user.is_staff,
+                "is_superuser": user.is_superuser,
+                "features_flag": [f.feature.code for f in user.featureflag_set.all()],
+            },
+        )
+    except Exception as e:
+        capture_exception(e)
