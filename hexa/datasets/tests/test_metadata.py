d03d879594ba5e2ec1d2f33b39f1bbe6f574dde1
nazarfil <nfilipchuk@bluesquarehub.com>
2024-07-16 13:48:01 +0200

    fix(Dataset): tests handled exceptions

--- hexa/datasets/tests/test_metadata.py
+++ hexa/datasets/tests/test_metadata.py
@@ -1,6 +1,8 @@
 import os
 from unittest import mock
 
+from pandas.errors import ParserError
+
 from hexa.core.test import TestCase
 from hexa.datasets.models import DatasetFileMetadata
 from hexa.datasets.queue import generate_dataset_file_metadata_task
@@ -18,10 +20,23 @@ class TestCreateDatasetFileMetadataTask(TestCase):
         mock_DatasetVersionFile_get,
     ):
         test_cases = [
-            ("example_names.csv", DatasetFileMetadata.STATUS_FINISHED),
-            ("example_names.parquet", DatasetFileMetadata.STATUS_FINISHED),
+            (
+                "example_names.csv",
+                DatasetFileMetadata.STATUS_FINISHED,
+                '[{"name":"Jack","surname":"Howard"},{"name":"Olivia","surname":"Brown"},{"name":"Lily","surname":"Evan',
+            ),
+            (
+                "example_names_2_lines.csv",
+                DatasetFileMetadata.STATUS_FINISHED,
+                '[{"name":"Liam","surname":"Smith"},{"name":"Joe","surname":"Doe"},{"name":"Joe","surname":"Doe"},{"nam',
+            ),
+            (
+                "example_names.parquet",
+                DatasetFileMetadata.STATUS_FINISHED,
+                '[{"name":"Jack","surname":"Howard"},{"name":"Olivia","surname":"Brown"},{"name":"Lily","surname":"Evan',
+            ),
         ]
-        for filename, expected_status in test_cases:
+        for filename, expected_status, expected_content in test_cases:
             with self.subTest(filename=filename):
                 dataset_version_file = mock.Mock()
                 dataset_version_file.id = 1
@@ -51,7 +66,6 @@ class TestCreateDatasetFileMetadataTask(TestCase):
                 )
                 dataset_file_metadata.save.assert_called()
                 self.assertEqual(dataset_file_metadata.status, expected_status)
-                expected_content = '[{"name":"Sofia","surname":"Walker"},{"name":"Aiden","surname":"Adams"},{"name":"Eleanor","surname":"H'
                 self.assertEqual(
                     dataset_file_metadata.sample[0 : len(expected_content)],
                     expected_content,
@@ -71,22 +85,34 @@ class TestCreateDatasetFileMetadataTask(TestCase):
         mock_DatasetFileMetadata_create,
         mock_DatasetVersionFile_get,
     ):
-        dataset_version_file = mock.Mock()
-        dataset_version_file.id = 1
-        dataset_version_file.filename = "example_names.csv"
-        mock_DatasetVersionFile_get.return_value = dataset_version_file
+        test_cases = [
+            (get_storage().exceptions.NotFound, DatasetFileMetadata.STATUS_FAILED),
+            (ValueError, DatasetFileMetadata.STATUS_FAILED),
+            (ParserError, DatasetFileMetadata.STATUS_FAILED),
+        ]
+        for exception, expected_status in test_cases:
+            with self.subTest(exception=exception):
+                dataset_version_file = mock.Mock()
+                dataset_version_file.id = 1
+                dataset_version_file.filename = "example_names.csv"
+                mock_DatasetVersionFile_get.return_value = dataset_version_file
 
-        dataset_file_metadata = mock.Mock()
-        mock_DatasetFileMetadata_create.return_value = dataset_file_metadata
+                dataset_file_metadata = mock.Mock()
+                mock_DatasetFileMetadata_create.return_value = dataset_file_metadata
 
-        mock_generate_download_url.side_effect = get_storage().exceptions.NotFound
+                mock_generate_download_url.side_effect = exception
 
-        job = mock.Mock()
-        job.args = {"file_id": dataset_version_file.id}
-        generate_dataset_file_metadata_task(mock.Mock(), job)
+                job = mock.Mock()
+                job.args = {"file_id": dataset_version_file.id}
+                generate_dataset_file_metadata_task(mock.Mock(), job)
 
-        mock_DatasetVersionFile_get.assert_called_with(id=dataset_version_file.id)
-        dataset_file_metadata.save.assert_called()
-        self.assertEqual(
-            dataset_file_metadata.status, DatasetFileMetadata.STATUS_FAILED
-        )
+                mock_DatasetVersionFile_get.assert_called_with(
+                    id=dataset_version_file.id
+                )
+                dataset_file_metadata.save.assert_called()
+                self.assertEqual(dataset_file_metadata.status, expected_status)
+
+                mock_generate_download_url.reset_mock()
+                mock_DatasetVersionFile_get.reset_mock()
+                mock_DatasetFileMetadata_create.reset_mock()
+                dataset_file_metadata.save.reset_mock()
