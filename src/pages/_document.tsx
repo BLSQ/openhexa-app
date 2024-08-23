308f816995eb3886b1210e30f9552d5e6bbaaaab
Quentin Gérôme <quentin@letsbuildit.dev>
2024-08-23 10:09:21 +0200

    chore: Remove Plausible (#681)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import BaseDocument, { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 class Document extends BaseDocument {
   render() {
@@ -7,13 +6,6 @@ class Document extends BaseDocument {
       <Html className="h-full bg-gray-100">
         <Head>
           <link rel="icon" href="/static/img/favicon.png" />
-          {process.env.TELEMETRY_ID && (
-            <Script
-              src="https://plausible.io/js/script.js"
-              strategy="lazyOnload"
-              data-domain={process.env.TELEMETRY_ID}
-            />
-          )}
         </Head>
         <body className="h-full">
           <Main />
