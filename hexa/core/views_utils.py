39d5ed6cb600ee5485dedadb59c84a4d6e935358
Quentin Gérôme <quentin@letsbuildit.dev>
2024-09-11 10:31:52 +0200

    fix(Statics): Remove cors decorator (#809)
    * chore(docker): set app image platform to linux/amd64
    
    * fix(CORS): Add a decorator to disable CORS on specific endpoints

--- hexa/core/views_utils.py
+++ hexa/core/views_utils.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from django.conf import settings
 from django.http import Http404, HttpRequest
 from django.shortcuts import redirect
@@ -11,3 +13,12 @@ def redirect_to_new_frontend(request: HttpRequest, *_, **__):
             )
         )
     raise Http404("Page not found")
+
+
+def disable_cors(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        request._cors_enabled = False
+        return view_func(request, *args, **kwargs)
+
+    return _wrapped_view
