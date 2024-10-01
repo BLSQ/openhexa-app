2c545aa82634f4183924268533d31b3c2da10e73
Quentin Gérôme <quentin@letsbuildit.dev>
2024-10-01 18:49:39 +0200

    fix(GCP): Bucket cannot contain '.' or be a recognized top-level domain (#820)

--- hexa/workspaces/models.py
+++ hexa/workspaces/models.py
@@ -83,7 +83,7 @@ def create_workspace_bucket(workspace_slug: str):
             # Bucket names must be unique across all of Google Cloud, so we add a suffix to the workspace slug
             # When separated by a dot, each segment can be up to 63 characters long
             return storage.create_bucket(
-                f"{(settings.WORKSPACE_BUCKET_PREFIX + workspace_slug)[:63]}.{suffix}",
+                f"{(settings.WORKSPACE_BUCKET_PREFIX + workspace_slug)[:63]}_{suffix}",
                 labels={"hexa-workspace": workspace_slug},
             )
         except ValidationError:
