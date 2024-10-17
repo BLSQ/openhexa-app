fc512d0a48866ee23996552783846ad80fdbcae9
Quentin Gérôme <geromequentin@gmail.com>
2024-10-17 10:26:08 +0200

    fix: CORS regex was not correct and the dev configuration was redefining unnecessary variables

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -3,8 +3,3 @@ from .base import *  # noqa: F403, F401
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 SECURE_SSL_REDIRECT = False
-ALLOWED_HOSTS = ["*"]
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
-CORS_URLS_REGEX = r"^/graphql/(\w+\/)?$"
-CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
