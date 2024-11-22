15c0e2256e4ddef0572303909a7b6f28e2dffa01
YolanFery <yolan@codethefuture.be>
2024-11-22 09:20:55 +0100

    Rename version name

--- hexa/pipelines/models.py
+++ hexa/pipelines/models.py
@@ -199,9 +199,7 @@ class PipelineVersion(models.Model):
 
     @property
     def version_name(self):
-        return f"Version {self.version_number}" + (
-            f" ({self.name})" if self.name else ""
-        )
+        return str(self.version_number) + f" [{self.name}]" if self.name else ""
 
     @property
     def display_name(self):
