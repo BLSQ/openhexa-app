e7e0f67541723caa1dc00a19537fb4a437158852
Quentin Gérôme <quentin@letsbuildit.dev>
2024-11-26 16:06:39 +0100

    chore: Add styling to LinkTabs

--- src/core/components/Tabs/LinkTabs/LinkTabs.tsx
+++ src/core/components/Tabs/LinkTabs/LinkTabs.tsx
@@ -5,9 +5,17 @@ type LinkTabProps = {
   selected?: string;
   tabs: { label: string; href: string; id: string }[];
   className?: string;
+  selectedStyle?: string;
+  style?: string;
 };
 
-const LinkTabs = ({ tabs, selected, className }: LinkTabProps) => {
+const LinkTabs = ({
+  tabs,
+  selected,
+  className,
+  selectedStyle = "text-gray-900 font-medium",
+  style = "text-gray-500 hover:border-gray-400",
+}: LinkTabProps) => {
   return (
     <div
       className={clsx("flex space-x-8 -mb-px text-sm font-medium", className)}
@@ -17,10 +25,11 @@ const LinkTabs = ({ tabs, selected, className }: LinkTabProps) => {
           key={tab.id}
           href={tab.href}
           className={clsx(
-            "whitespace-nowrap cursor-pointer border-b-2 px-1.5 pt-2.5 pb-2 tracking-wide first:pl-0  first:ml-1.5 hover:text-gray-900",
+            "whitespace-nowrap cursor-pointer border-b-2 px-1.5 pt-2.5 pb-2 tracking-wide first:pl-0 first:ml-1.5 ",
+            style,
             tab.id === selected
-              ? "border-pink-500 text-gray-900 font-medium"
-              : "border-transparent text-gray-500 hover:border-gray-400",
+              ? `border-pink-500 ${selectedStyle}`
+              : `border-transparent`,
           )}
         >
           {tab.label}
