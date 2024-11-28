2fe23993c729df453743a2321698208acf60d336
Nazar F <nfilipchuk@bluesquarehub.com>
2024-11-28 10:13:10 +0100

    fix: skips geometry and processes wkb (#858)
    * fix: skips geometry and processes wkb
    
    * removed repeated test
    
    * fix: skipped bytes
    
    * fix: skipped bytes

---
+++ hexa/datasets/tests/fixtures/wkb_geometry_encoded.py
@@ -0,0 +1,14 @@
+wkb_geometry = {
+    "bbox": {
+        "xmax": -66.96465999999998,
+        "xmin": -171.79111060289122,
+        "ymax": 71.35776357694175,
+        "ymin": 18.91619,
+    },
+    "geometry": "<SKIPPED_BYTES>",
+    "name": "United States of America",
+    "iso_a3": "USA",
+    "pop_est": 328239523.0,
+    "continent": "North America",
+    "gdp_md_est": 21433226,
+}
