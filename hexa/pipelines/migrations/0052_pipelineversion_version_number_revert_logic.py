c4195d600314730a2468f7b3404df2f5680f9283
YolanFery <yolan@codethefuture.be>
2024-11-28 15:30:43 +0100

    Add a revert script in case of empty values

---
+++ hexa/pipelines/migrations/0052_pipelineversion_version_number_revert_logic.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+from django.db.models import F, Value
+from django.db.models.functions import Concat
+
+
+def fill_empty_names(apps, _):
+    PipelineVersion = apps.get_model("pipelines", "PipelineVersion")
+    PipelineVersion.objects.filter(name__isnull=True).update(
+        name=Concat(
+            Value("Version "), F("version_number"), Value(" (auto-generated name)")
+        )
+    )
+
+
+class Migration(migrations.Migration):
+    dependencies = [("pipelines", "0051_pipelineversion_version_number_cleanup")]
+
+    operations = [
+        migrations.RunPython(migrations.RunPython.noop, reverse_code=fill_empty_names),
+    ]
