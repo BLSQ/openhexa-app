2b30d1c54be985d08f8333db05c8ae2edcdbf5cb
YolanFery <yolan@codethefuture.be>
2024-12-04 16:30:16 +0100

    Upgrade recommended

--- hexa/workspaces/schema/queries.py
+++ hexa/workspaces/schema/queries.py
@@ -13,12 +13,12 @@ workspace_queries = QueryType()
 
 
 @workspace_queries.field("workspaces")
-def resolve_workspaces(_, info, query=None, page=1, perPage=15):
+def resolve_workspaces(_, info, query=None, page=1, per_page=15):
     request = info.context["request"]
     queryset = Workspace.objects.filter_for_user(request.user).order_by("-updated_at")
     if query is not None:
         queryset = queryset.filter(name__icontains=query)
-    return result_page(queryset=queryset, page=page, per_page=perPage)
+    return result_page(queryset=queryset, page=page, per_page=per_page)
 
 
 @workspace_queries.field("workspace")
@@ -44,7 +44,7 @@ def resolve_workspace_connection_by_slug(_, info, **kwargs):
     request = info.context["request"]
     try:
         return Connection.objects.filter_for_user(request.user).get(
-            workspace__slug=kwargs["workspaceSlug"], slug=kwargs["connectionSlug"]
+            workspace__slug=kwargs["workspace_slug"], slug=kwargs["connection_slug"]
         )
     except Connection.DoesNotExist:
         return None
