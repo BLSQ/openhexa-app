72f959c6b9ec5fd39a87ba8e2bc139b8aaa625dc
YolanFery <yolan@codethefuture.be>
2024-12-12 18:00:37 +0100

    Fix bug and remove paging

--- hexa/template_pipelines/schema/types.py
+++ hexa/template_pipelines/schema/types.py
@@ -1,6 +1,5 @@
 from ariadne import ObjectType
 
-from hexa.core.graphql import result_page
 from hexa.template_pipelines.models import Template
 from hexa.workspaces.models import Workspace
 from hexa.workspaces.schema.types import workspace_permissions
@@ -23,11 +22,8 @@ def resolve_workspace_permissions_create_template_version(
 
 
 @template_object.field("versions")
-def resolve_pipeline_versions(template: Template, info, **kwargs):
-    qs = template.versions.all()
-    return result_page(
-        queryset=qs, page=kwargs.get("page", 1), per_page=kwargs.get("per_page")
-    )
+def resolve_template_versions(template: Template, info, **kwargs):
+    return template.versions.all()
 
 
 bindables = [
