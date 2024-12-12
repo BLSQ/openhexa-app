4b35a23c44d012f247339ab824cd5e050de64758
YolanFery <yolan@codethefuture.be>
2024-12-12 09:48:12 +0100

    Permissisions

---
+++ hexa/template_pipelines/permissions.py
@@ -0,0 +1,9 @@
+from hexa.user_management.models import User
+from hexa.workspaces.models import Workspace, WorkspaceMembershipRole
+
+
+def create_template(principal: User, workspace: Workspace):
+    return workspace.workspacemembership_set.filter(
+        user=principal,
+        role__in=[WorkspaceMembershipRole.EDITOR, WorkspaceMembershipRole.ADMIN],
+    ).exists()
