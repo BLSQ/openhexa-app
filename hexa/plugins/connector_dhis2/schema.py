83cd620284efb9bb57e29ab473d1802242f4e9a0
nazarfil <nfilipchuk@bluesquarehub.com>
2024-12-12 15:20:26 +0100

    refactor: decomission dhis2_connector

--- hexa/plugins/connector_dhis2/schema.py
+++ hexa/plugins/connector_dhis2/schema.py
@@ -1,7 +1,6 @@
 from ariadne import MutationType, ObjectType, QueryType
 
 from hexa.core.graphql import load_type_defs_from_file
-from hexa.plugins.connector_dhis2.models import DataElement
 
 base_type_defs = load_type_defs_from_file(
     "plugins/connector_dhis2/graphql/schema.graphql"
@@ -15,12 +14,6 @@ mutations = MutationType()
 instance_object = ObjectType("DHIS2Instance")
 
 data_element_object = ObjectType("DHIS2DataElement")
-
-
-@data_element_object.field("instance")
-def resolve_data_element_instance(object: DataElement, info):
-    return object.instance
-
 
 dhis2_type_defs = [base_type_defs]
 dhis2_bindables = [
