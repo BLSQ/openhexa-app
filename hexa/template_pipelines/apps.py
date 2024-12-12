981624c180f528b9bbbb8f86ae689c4e33fe6473
YolanFery <yolan@codethefuture.be>
2024-12-12 10:59:40 +0100

    Directory cleanup

--- hexa/template_pipelines/apps.py
+++ hexa/template_pipelines/apps.py
@@ -4,6 +4,3 @@ from hexa.app import CoreAppConfig
 class TemplatePipelinesConfig(CoreAppConfig):
     name = "hexa.template_pipelines"
     label = "template_pipelines"
-
-    def ready(self):
-        from . import signals  # noqa
