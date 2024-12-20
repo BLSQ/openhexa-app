7a57cb33b02982956cab383646fa9da575ad0a8d
nazarfil <nfilipchuk@bluesquarehub.com>
2024-12-20 10:46:35 +0100

    added todos for toolbox

--- hexa/connections/tests/fixtures/org_units.py
+++ hexa/connections/tests/fixtures/org_units.py
@@ -2,85 +2,39 @@ org_units = [
     {
         "id": "Rp268JB6Ne4",
         "name": "Adonkia CHP",
-        "level": 4,
-        "path": "/ImspTQPwCqd/at6UHUQatSo/qtr8GGlm4gg/Rp268JB6Ne4",
-        "geometry": None,
     },
     {
         "id": "cDw53Ej8rju",
         "name": "Afro Arab Clinic",
-        "level": 4,
-        "path": "/ImspTQPwCqd/at6UHUQatSo/qtr8GGlm4gg/cDw53Ej8rju",
-        "geometry": None,
     },
     {
         "id": "GvFqTavdpGE",
         "name": "Agape CHP",
-        "level": 4,
-        "path": "/ImspTQPwCqd/O6uvpzGd5pu/U6Kr7Gtpidn/GvFqTavdpGE",
-        "geometry": None,
     },
-    {
-        "id": "plnHVbJR6p4",
-        "name": "Ahamadyya Mission Cl",
-        "level": 4,
-        "path": "/ImspTQPwCqd/PMa2VCrupOd/QywkxFudXrC/plnHVbJR6p4",
-        "geometry": '{"type": "Point", "coordinates": [-12.9487, 9.0131]}',
-    },
+    {"id": "plnHVbJR6p4", "name": "Ahamadyya Mission Cl"},
 ]
+
 org_units_groups = [
-    [
-        {
-            "id": "oDkJh5Ddh7d",
-            "name": "Acute Flaccid Paralysis (AFP) ",
-            "data_elements": [
-                "P3jJH5Tu5VC",
-                "M62VHgYT2n0",
-                "FQ2o8UBlcrS",
-                "eMyVanycQSC",
-                "HLPuaFB7Frw",
-                "RUv0hqER0zV",
-                "jmWyJFtE7Af",
-            ],
-        },
-        {"id": "GBHN1a1Jddh", "name": "All Others", "data_elements": ["qrur9Dvnyt5"]},
-        {
-            "id": "KmwPVkjp7yl",
-            "name": "Anaemia",
-            "data_elements": [
-                "HLPuaFB7Frw",
-                "eMyVanycQSC",
-                "yqBkn9CWKih",
-                "hCVSHjcml9g",
-                "jmWyJFtE7Af",
-            ],
-        },
-        {
-            "id": "qfxEYY9xAl6",
-            "name": "ANC",
-            "data_elements": [
-                "V37YqbqpEhV",
-                "hfdmMSPBgLG",
-                "bqK6eSIwo3h",
-                "yTHydhurQQU",
-                "Jtf34kNZhzP",
-                "cYeuwXTCPkU",
-                "fbfJHSPpUQD",
-                "SA7WeFZnUci",
-            ],
-        },
-        {
-            "id": "M2cth8EmrlT",
-            "name": "ARI treated with antibiotics (Pneumonia)",
-            "data_elements": ["iKGjnOOaPlE", "XTqOHygxDj5", "FHD3wiSM7Sn"],
-        },
-        {
-            "id": "yhg8oYU9ekY",
-            "name": "ARI Treated Without Antibiotics (Cough)",
-            "data_elements": ["Cm4XUw6VAxv", "oLfWYAJhZb2", "RF4VFVGdFRO"],
-        },
-    ]
+    {"name": "CHC", "id": "CXw2yu5fodb"},
+    {"name": "Chiefdom", "id": "gzcv65VyaGq"},
+    {"name": "CHP", "id": "uYxK4wmcPqA"},
+    {"name": "Clinic", "id": "RXL3lPSK8oG"},
+    {"name": "Country", "id": "RpbiCJpIYEj"},
+    {"name": "District", "id": "w1Atoz18PCL"},
+    {"name": "Eastern Area", "id": "nlX2VoouN63"},
+    {"name": "Hospital", "id": "tDZVQ1WtwpA"},
+    {"name": "MCHP", "id": "EYbopBOJWsW"},
+    {"name": "Mission", "id": "w0gFTTmsUcF"},
+    {"name": "NGO", "id": "PVLOW4bCshG"},
+    {"name": "Northern Area", "id": "J40PpdN4Wkk"},
+    {"name": "Private Clinic", "id": "MAs88nJc9nL"},
+    {"name": "Public facilities", "id": "oRVt7g429ZO"},
+    {"name": "Rural", "id": "GGghZsfu7qV"},
+    {"name": "Southern Area", "id": "jqBqIXoXpfy"},
+    {"name": "Urban", "id": "f25dqv3Y7Z0"},
+    {"name": "Western Area", "id": "b0EsAxm8Nge"},
 ]
+
 org_units_levels = [
     {"id": "H1KlN4QIauv", "level": 1, "name": "National"},
     {"id": "wjP19dkFeIk", "level": 2, "name": "District"},
--- hexa/connections/tests/test_dhis2connection.py
+++ hexa/connections/tests/test_dhis2connection.py
@@ -1,6 +1,19 @@
 from unittest.mock import patch
 
+import responses
+
 from hexa.connections.dhis2_connection import get_client_by_slug, get_dhis2_metadata
+from hexa.connections.tests.fixtures.data_elements import (
+    data_element_groups,
+    data_elements_by_data_elements_group,
+    datasets,
+)
+from hexa.connections.tests.fixtures.indicators import indicator_groups, indicators
+from hexa.connections.tests.fixtures.org_units import (
+    org_units,
+    org_units_groups,
+    org_units_levels,
+)
 from hexa.core.test import TestCase
 from hexa.user_management.models import User
 from hexa.workspaces.models import (
@@ -56,7 +69,7 @@ class TestDHIS2Methods(TestCase):
             [
                 {
                     "code": "url",
-                    "value": "https://play.im.dhis2.org/stable-2-41-2",
+                    "value": "http://127.0.0.1:8080",
                     "secret": False,
                 },
                 {"code": "username", "value": "admin", "secret": False},
@@ -65,56 +78,173 @@ class TestDHIS2Methods(TestCase):
         )
         connection.save()
 
+    @responses.activate
     def test_dhis2_connection_from_slug(self):
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080/api/system/ping",
+            status=200,
+        )
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080/api/system/info",
+            json={"version": "2.35"},
+            status=200,
+        )
         dhis2 = get_client_by_slug("dhis2-connection-1")
         self.assertIsNotNone(dhis2)
+        return dhis2
 
+    @responses.activate
     def test_dhis2_org_units(self):
-        dhis2 = get_client_by_slug("dhis2-connection-1")
-        metadata = get_dhis2_metadata(dhis2, "organisation_units", "id,name", None)
-        print(metadata)
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080/api/organisationUnits?fields=id%2Cname%2Clevel%2Cpath%2Cgeometry&pageSize=1000",
+            json={"organisationUnits": org_units},
+            status=200,
+        )
+        dhis2 = self.test_dhis2_connection_from_slug()
+        metadata = get_dhis2_metadata(dhis2, "organisation_units")
+        self.assertIsNotNone(metadata)
 
+    @responses.activate
+    def test_dhis2_org_units_by_groups(self):
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080/api/organisationUnits?fields=id%2Cname%2Clevel%2Cpath%2Cgeometry&filter=organisationUnitGroups.id%3Ain%3A%5BoDkJh5Ddh7d%5D&pageSize=1000",
+            json={"organisationUnits": org_units},
+            status=200,
+        )
+        dhis2 = self.test_dhis2_connection_from_slug()
+        metadata = get_dhis2_metadata(
+            dhis2,
+            "organisation_units",
+            filter="organisationUnitGroups.id:in:[oDkJh5Ddh7d]",
+        )
+        self.assertIsNotNone(metadata)
+
+    @responses.activate
     def test_dhis2_org_unit_groups(self):
-        dhis2 = get_client_by_slug("dhis2-connection-1")
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080//api/organisationUnits?fields=id%2Cname%2Clevel%2Cpath%2Cgeometry&filter=organisationUnitGroups.id%3Ain%3A%5BoDkJh5Ddh7d%5D&pageSize=1000",
+            json={"organisationUnitGroups": org_units_groups},
+            status=200,
+        )
+        dhis2 = self.test_dhis2_connection_from_slug()
         metadata = get_dhis2_metadata(dhis2, type="organisation_unit_groups")
         self.assertIsNotNone(metadata)
-        print(metadata)
 
+    @responses.activate
     def test_dhis2_org_unit_levels(self):
-        dhis2 = get_client_by_slug("dhis2-connection-1")
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080/api/filledOrganisationUnitLevels",
+            json={"organisationUnitLevels": org_units_levels},
+            status=200,
+        )
+        dhis2 = self.test_dhis2_connection_from_slug()
         metadata = get_dhis2_metadata(dhis2, "organisation_unit_levels")
         self.assertIsNotNone(metadata)
-        print(metadata)
 
+    @responses.activate
     def test_dhis2_datasets(self):
-        dhis2 = get_client_by_slug("dhis2-connection-1")
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080/api/dataSets?fields=id%2Cname%2CdataSetElements%2Cindicators%2CorganisationUnits&pageSize=1000",
+            json={"dataSets": datasets},
+            status=200,
+        )
+        dhis2 = self.test_dhis2_connection_from_slug()
         metadata = get_dhis2_metadata(dhis2, "datasets")
         self.assertIsNotNone(metadata)
-        print(metadata)
 
+    @responses.activate
     def test_dhis2_data_elements(self):
-        dhis2 = get_client_by_slug("dhis2-connection-1")
-        # metadata = get_dhis2_metadata(dhis2, "data_elements", filter="dataElementGroups.id:in:[oDkJh5Ddh7d]")
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080/api/dataElements?fields=id%2Cname%2CaggregationType%2CzeroIsSignificant&pageSize=1000",
+            json={"dataElements": data_elements_by_data_elements_group},
+            status=200,
+        )
+        dhis2 = self.test_dhis2_connection_from_slug()
+        metadata = get_dhis2_metadata(dhis2, "data_elements")
+        self.assertIsNotNone(metadata)
+
+    @responses.activate
+    def test_dhis2_data_elements_by_datasets(self):
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080",
+            json={"dataElements": data_elements_by_data_elements_group},
+            status=200,
+        )
+        dhis2 = self.test_dhis2_connection_from_slug()
         metadata = get_dhis2_metadata(
             dhis2, "data_elements", filter="dataSetElements.dataSet.id:in:[lyLU2wR22tC]"
         )
         self.assertIsNotNone(metadata)
-        print(metadata)
 
+    @responses.activate
+    def test_dhis2_data_elements_by_groups(self):
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080/api/dataElements",
+            json={"dataElements": data_elements_by_data_elements_group},
+            status=200,
+        )
+        dhis2 = self.test_dhis2_connection_from_slug()
+        metadata = get_dhis2_metadata(
+            dhis2, "data_elements", filter="dataElementGroups.id:in:[oDkJh5Ddh7d]"
+        )
+        self.assertIsNotNone(metadata)
+
+    @responses.activate
     def test_dhis2_data_element_groups(self):
-        dhis2 = get_client_by_slug("dhis2-connection-1")
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080/api/dataElementGroups",
+            json={"dataElementGroups": data_element_groups},
+            status=200,
+        )
+        dhis2 = self.test_dhis2_connection_from_slug()
         metadata = get_dhis2_metadata(dhis2, "data_element_groups")
         self.assertIsNotNone(metadata)
-        print(metadata)
 
+    @responses.activate
     def test_dhis2_indicators(self):
-        dhis2 = get_client_by_slug("dhis2-connection-1")
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080/api/indicators",
+            json={"indicators": indicators},
+            status=200,
+        )
+        dhis2 = self.test_dhis2_connection_from_slug()
         metadata = get_dhis2_metadata(dhis2, "indicator")
         self.assertIsNotNone(metadata)
-        print(metadata)
 
+    @responses.activate
+    def test_dhis2_indicators_by_groups(self):
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080/api/indicators",
+            json={"indicators": indicators},
+            status=200,
+        )
+        dhis2 = self.test_dhis2_connection_from_slug()
+        metadata = get_dhis2_metadata(
+            dhis2, "indicator", filter="indicatorGroups.id:in:[PoTnGN0F2n5]"
+        )
+        self.assertIsNotNone(metadata)
+
+    @responses.activate
     def test_dhis2_indicator_groups(self):
-        dhis2 = get_client_by_slug("dhis2-connection-1")
+        responses.add(
+            responses.GET,
+            "http://127.0.0.1:8080/api/indicatorGroups",
+            json={"indicatorGroups": indicator_groups},
+            status=200,
+        )
+        dhis2 = self.test_dhis2_connection_from_slug()
         metadata = get_dhis2_metadata(dhis2, "indicator_groups")
         self.assertIsNotNone(metadata)
-        print(metadata)
