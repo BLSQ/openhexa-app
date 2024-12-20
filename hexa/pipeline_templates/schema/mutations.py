47e89d9672f308a5954406c25af5702c5f31f2ec
Yolan Fery <yolan@codethefuture.be>
2024-12-20 08:27:48 +0100

    Update hexa/pipeline_templates/schema/mutations.py
    Co-authored-by: Quentin Gérôme <quentin@letsbuildit.dev>

--- hexa/pipeline_templates/schema/mutations.py
+++ hexa/pipeline_templates/schema/mutations.py
@@ -44,7 +44,7 @@ def resolve_create_pipeline_template_version(_, info, **kwargs):
     ):
         raise PermissionDenied()
 
-    source_pipeline = get_source_pipeline(request.user, input.get("pipeline_id"))
+    source_pipeline = get_source_pipeline(request.user, input["pipeline_id"])
     if not source_pipeline:
         return {"success": False, "errors": ["PIPELINE_NOT_FOUND"]}
 
