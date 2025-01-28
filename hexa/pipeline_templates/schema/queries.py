4b8788553048dd8a4bc43bcd56557277a00be90a
YolanFery <yolan@codethefuture.be>
2025-01-28 15:48:24 +0100

    Filter

--- hexa/pipeline_templates/schema/queries.py
+++ hexa/pipeline_templates/schema/queries.py
@@ -1,15 +1,29 @@
 from ariadne import QueryType
+from django.db.models import Q
+from django.http import HttpRequest
 
 from hexa.core.graphql import result_page
 from hexa.pipeline_templates.models import PipelineTemplate
+from hexa.workspaces.models import Workspace
 
 pipeline_template_query = QueryType()
 
 
 @pipeline_template_query.field("pipelineTemplates")
 def resolve_pipeline_templates(_, info, **kwargs):
+    request: HttpRequest = info.context["request"]
     search = kwargs.get("search", "")
-    pipeline_templates = PipelineTemplate.objects.filter(name__icontains=search)
+
+    pipeline_templates = PipelineTemplate.objects.filter(
+        Q(name__icontains=search) | Q(description__icontains=search)
+    )
+
+    workspace_slug = kwargs.get("workspace_slug")
+    if workspace_slug:
+        workspace: Workspace = Workspace.objects.filter_for_user(request.user).get(
+            slug=workspace_slug
+        )
+        pipeline_templates = pipeline_templates.filter(workspace=workspace)
 
     return result_page(
         pipeline_templates,
