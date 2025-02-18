9bdc1a34a76b8a8ce446f857923ae14a0eca8ea9
Yolan Fery <yolan@codethefuture.be>
2025-02-19 00:47:55 +0100

    fix: Glitch when opening, choosing a tab and then closing the pipeline create dialog with feature flag enabled (#1107)
    Fix small glitch when closing/opening

--- src/workspaces/features/CreatePipelineDialog/CreatePipelineDialog.tsx
+++ src/workspaces/features/CreatePipelineDialog/CreatePipelineDialog.tsx
@@ -117,7 +117,7 @@ const CreatePipelineDialog = (props: CreatePipelineDialogProps) => {
     <Dialog open={open} onClose={onClose} maxWidth="max-w-6xl">
       <Dialog.Title>{t("How to create a pipeline")}</Dialog.Title>
       <Dialog.Content className="space-y-4">
-        <Tabs onChange={(index) => setTabIndex(index)}>
+        <Tabs onChange={(index) => setTabIndex(index)} defaultIndex={tabIndex}>
           {pipelineTemplateFeatureEnabled && (
             <Tabs.Tab label={t("From Template")} className={"space-y-2 pt-2"}>
               <PipelineTemplateTable workspace={workspace} />
