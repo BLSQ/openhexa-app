ccd40f6dfc6d93ec0d4b66642c958ef0a6068c8b
YolanFery <yolan@codethefuture.be>
2025-02-24 11:04:50 +0100

    Fix

--- hexa/pipeline_templates/models.py
+++ hexa/pipeline_templates/models.py
@@ -186,7 +186,9 @@ class PipelineTemplateVersion(models.Model):
             "config": source_pipeline.config,
         }
         return Pipeline.objects.create_with_unique_code(
-            name=source_pipeline.code, workspace=workspace, **data
+            name=source_pipeline.name or source_pipeline.code,
+            workspace=workspace,
+            **data,
         )
 
     def _extract_config(self, pipeline: Pipeline) -> dict:
