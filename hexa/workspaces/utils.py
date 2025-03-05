e08a87e4745947673fdfb97dec9cd203117f84d9
Quentin Gérôme <quentin@letsbuildit.dev>
2025-03-05 08:17:27 +0100

    WIP better types

--- hexa/workspaces/utils.py
+++ hexa/workspaces/utils.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import StrEnum
 from typing import Optional
 from urllib.parse import urlencode
@@ -9,7 +10,7 @@ from openhexa.toolbox.dhis2 import DHIS2
 from hexa.core.utils import send_mail
 from hexa.user_management.models import User
 
-from .models import Connection, WorkspaceInvitation
+from .models import Connection, ConnectionType, WorkspaceInvitation
 
 
 def send_workspace_invitation_email(
@@ -43,9 +44,9 @@ def send_workspace_invitation_email(
 
 
 class DHIS2MetadataQueryType(StrEnum):
-    ORGANISATION_UNITS = "ORGANISATION_UNITS"
-    ORGANISATION_UNIT_GROUPS = "ORGANISATION_UNIT_GROUPS"
-    ORGANISATION_UNIT_LEVELS = "ORGANISATION_UNIT_LEVELS"
+    ORG_UNITS = "ORG_UNITS"
+    ORG_UNIT_GROUPS = "ORG_UNIT_GROUPS"
+    ORG_UNIT_LEVELS = "ORG_UNIT_LEVELS"
     DATASETS = "DATASETS"
     DATA_ELEMENTS = "DATA_ELEMENTS"
     DATA_ELEMENT_GROUPS = "DATA_ELEMENT_GROUPS"
@@ -53,29 +54,59 @@ class DHIS2MetadataQueryType(StrEnum):
     INDICATOR_GROUPS = "INDICATOR_GROUPS"
 
 
+@dataclass
+class DHIS2MetadataResponse:
+    items: list[dict]
+    total_items: int
+    total_pages: int
+    page_number: int
+
+
 def query_dhis2_metadata(
-    dhis2: DHIS2, query_type: DHIS2MetadataQueryType, **kwargs
-) -> dict:
+    dhis2_client: DHIS2, query_type: DHIS2MetadataQueryType, **kwargs
+) -> DHIS2MetadataResponse:
     """
     Gets metadata from DHIS2
     """
     metadata_methods = {
-        DHIS2MetadataQueryType.ORGANISATION_UNITS: dhis2.meta.organisation_units,
-        DHIS2MetadataQueryType.ORGANISATION_UNIT_GROUPS: dhis2.meta.organisation_unit_groups,
-        DHIS2MetadataQueryType.ORGANISATION_UNIT_LEVELS: dhis2.meta.organisation_unit_levels,
-        DHIS2MetadataQueryType.DATASETS: dhis2.meta.datasets,
-        DHIS2MetadataQueryType.DATA_ELEMENTS: dhis2.meta.data_elements,
-        DHIS2MetadataQueryType.DATA_ELEMENT_GROUPS: dhis2.meta.data_element_groups,
-        DHIS2MetadataQueryType.INDICATORS: dhis2.meta.indicators,
-        DHIS2MetadataQueryType.INDICATOR_GROUPS: dhis2.meta.indicator_groups,
+        DHIS2MetadataQueryType.ORG_UNITS: dhis2_client.meta.organisation_units,
+        DHIS2MetadataQueryType.ORG_UNIT_GROUPS: dhis2_client.meta.organisation_unit_groups,
+        DHIS2MetadataQueryType.ORG_UNIT_LEVELS: dhis2_client.meta.organisation_unit_levels,
+        DHIS2MetadataQueryType.DATASETS: dhis2_client.meta.datasets,
+        DHIS2MetadataQueryType.DATA_ELEMENTS: dhis2_client.meta.data_elements,
+        DHIS2MetadataQueryType.DATA_ELEMENT_GROUPS: dhis2_client.meta.data_element_groups,
+        DHIS2MetadataQueryType.INDICATORS: dhis2_client.meta.indicators,
+        DHIS2MetadataQueryType.INDICATOR_GROUPS: dhis2_client.meta.indicator_groups,
     }
-    return metadata_methods[query_type](**kwargs)
+    response = metadata_methods[query_type](**kwargs)
+
+    if isinstance(response, dict):
+        pager = response.get("pager", {})
+        return DHIS2MetadataResponse(
+            items=response.get("items", []),
+            total_items=pager.get("total", len(response.get("items", []))),
+            total_pages=pager.get("pageCount", 1),
+            page_number=pager.get("page", 1),
+        )
+
+    elif isinstance(response, list):
+        return DHIS2MetadataResponse(
+            items=response,
+            total_items=len(response),
+            total_pages=1,
+            page_number=1,
+        )
+    else:
+        raise ValueError("Unexpected response format from DHIS2")
 
 
 def dhis2_client_from_connection(connection: Connection) -> DHIS2 | None:
     """
     Gets DHIS2 toolbox client from workspace connection
     """
+    if connection.connection_type != ConnectionType.DHIS2:
+        raise ValueError("Connection is not a DHIS2 connection")
+
     fields = connection.fields.all()
     dhis_connection_dict = {field.code: field.value for field in fields}
 
@@ -97,34 +128,3 @@ def generate_search_filter(kwargs):
             filters.append(f"{item['field']}:in[{','.join(map(str, item['value']))}]")
 
     return filters
-
-
-def process_metadata_response(metadata_response, page):
-    if isinstance(metadata_response, dict):
-        metadata_items = metadata_response.get("items", [])
-        pager = metadata_response.get("pager", {})
-        total_items = pager.get("total", len(metadata_items))
-        total_pages = pager.get("pageCount", 1)
-        page_number = pager.get("page", page)
-
-    elif isinstance(metadata_response, list):
-        metadata_items = metadata_response
-        total_items = len(metadata_items)
-        total_pages = 1
-        page_number = page
-
-    else:
-        raise ValueError("Unexpected response format from DHIS2")
-
-    return metadata_items, total_items, total_pages, page_number
-
-
-def error_response(page, error_type):
-    return {
-        "items": [],
-        "total_items": 0,
-        "total_pages": 0,
-        "page_number": page,
-        "success": False,
-        "error": error_type,
-    }
