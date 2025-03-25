ad0c99a0ebd41e19a0e41359ef44c38f33e52c65
nazarfil <nfilipchuk@bluesquarehub.com>
2025-03-25 18:00:54 +0100

    add translation

--- hexa/core/utils.py
+++ hexa/core/utils.py
@@ -2,6 +2,7 @@ import typing
 
 from django.core.mail import send_mail as django_send_mail
 from django.template.loader import render_to_string
+from mjml import mjml2html
 
 
 def send_mail(
@@ -21,6 +22,26 @@ def send_mail(
         template_variables,
     )
 
+    django_send_mail(
+        title,
+        message=text_message,
+        html_message=html_message,
+        recipient_list=recipient_list,
+        from_email=None,
+    )
+
+
+def send_email_mjml(
+    *,
+    title: str,
+    recipient_list: typing.Sequence[str],
+    template_name: str,
+    template_variables: typing.Mapping,
+):
+    text_message = render_to_string(f"{template_name}.txt", template_variables)
+    html_message = mjml2html(
+        render_to_string(f"{template_name}.mjml", template_variables)
+    )
     django_send_mail(
         title,
         message=text_message,
