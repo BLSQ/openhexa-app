b62855682286e219435cf2e5c0430ed0d959dcc6
YolanFery <yolan@codethefuture.be>
2025-04-24 11:25:26 +0200

    Workaround

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -17,5 +17,13 @@ const customJestConfig = {
   restoreMocks: true,
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
+// See : https://github.com/vercel/next.js/issues/35634#issuecomment-1115250297
+async function jestConfig() {
+  const nextJestConfig = await createJestConfig(customJestConfig)();
+  // /node_modules/ is the first pattern
+  nextJestConfig.transformIgnorePatterns[0] =
+    "/node_modules/(?!react-hotkeys-hook)/";
+  return nextJestConfig;
+}
+
+module.exports = jestConfig;
