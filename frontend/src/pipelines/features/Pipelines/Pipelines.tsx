16a2f6387e048881a51a7799587838b695d6971a
YolanFery <yolan@codethefuture.be>
2025-04-29 16:40:28 +0200

    Header

--- frontend/src/pipelines/features/Pipelines/Pipelines.tsx
+++ frontend/src/pipelines/features/Pipelines/Pipelines.tsx
@@ -7,15 +7,20 @@ import Pagination from "core/components/Pagination";
 import { gql } from "@apollo/client";
 import { Pipelines_WorkspaceFragment } from "./Pipelines.generated";
 import { useWorkspacePipelinesPageQuery } from "workspaces/graphql/queries.generated";
+import Header from "../PipelineTemplates/Header";
 
 type PipelinesProps = {
   workspace: Pipelines_WorkspaceFragment;
   setDialogOpen: (open: boolean) => void;
 };
 
+// TODO : backend search + wiring
+
 const Pipelines = ({ workspace, setDialogOpen }: PipelinesProps) => {
   const { t } = useTranslation();
   const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [view, setView] = useState<"grid" | "card">("grid");
   const [page, setPage] = useState(1);
   const [perPage, setPerPage] = useState(10);
 
@@ -33,45 +38,59 @@ const Pipelines = ({ workspace, setDialogOpen }: PipelinesProps) => {
 
   const { pipelines } = data;
 
-  return pipelines.items.length === 0 ? (
-    <div className="text-center text-gray-500">
-      <div>{t("This workspace does not have any pipeline.")}</div>
-      <Button
-        variant="secondary"
-        onClick={() => setDialogOpen(true)}
-        className="mt-4"
-      >
-        {t("Add a new pipeline")}
-      </Button>
-    </div>
-  ) : (
-    <>
-      <div className="mt-5 mb-3 grid grid-cols-2 gap-4 xl:grid-cols-3 xl:gap-5">
-        {pipelines.items.map((pipeline, index) => (
-          <PipelineCard workspace={workspace} key={index} pipeline={pipeline} />
-        ))}
-      </div>
-      <Pagination
-        onChange={(page, perPage) => {
-          setPage(page);
-          setPerPage(perPage);
-          router
-            .push({
-              pathname: "/workspaces/[workspaceSlug]/pipelines",
-              query: {
-                page,
-                perPage,
-                workspaceSlug: workspace.slug,
-              },
-            })
-            .then();
-        }}
-        page={page}
-        perPage={perPage}
-        totalItems={pipelines.totalItems}
-        countItems={pipelines.items.length}
+  return (
+    <div>
+      <Header
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        view={view}
+        setView={setView}
       />
-    </>
+      {pipelines.items.length === 0 ? (
+        <div className="text-center text-gray-500">
+          <div>{t("This workspace does not have any pipeline.")}</div>
+          <Button
+            variant="secondary"
+            onClick={() => setDialogOpen(true)}
+            className="mt-4"
+          >
+            {t("Add a new pipeline")}
+          </Button>
+        </div>
+      ) : (
+        <>
+          <div className="mt-5 mb-3 grid grid-cols-2 gap-4 xl:grid-cols-3 xl:gap-5">
+            {pipelines.items.map((pipeline, index) => (
+              <PipelineCard
+                workspace={workspace}
+                key={index}
+                pipeline={pipeline}
+              />
+            ))}
+          </div>
+          <Pagination
+            onChange={(page, perPage) => {
+              setPage(page);
+              setPerPage(perPage);
+              router
+                .push({
+                  pathname: "/workspaces/[workspaceSlug]/pipelines",
+                  query: {
+                    page,
+                    perPage,
+                    workspaceSlug: workspace.slug,
+                  },
+                })
+                .then();
+            }}
+            page={page}
+            perPage={perPage}
+            totalItems={pipelines.totalItems}
+            countItems={pipelines.items.length}
+          />
+        </>
+      )}
+    </div>
   );
 };
 
