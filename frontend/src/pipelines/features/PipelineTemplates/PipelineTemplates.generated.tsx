3cd610ea251390b2f73d22a3d2038b8ea3a9302e
YolanFery <yolan@codethefuture.be>
2025-05-28 10:25:30 +0200

    Get workspace name slug

--- frontend/src/pipelines/features/PipelineTemplates/PipelineTemplates.generated.tsx
+++ frontend/src/pipelines/features/PipelineTemplates/PipelineTemplates.generated.tsx
@@ -12,7 +12,7 @@ export type GetPipelineTemplatesQueryVariables = Types.Exact<{
 }>;
 
 
-export type GetPipelineTemplatesQuery = { __typename?: 'Query', pipelineTemplates: { __typename?: 'PipelineTemplatePage', pageNumber: number, totalPages: number, totalItems: number, items: Array<{ __typename?: 'PipelineTemplate', id: string, description?: string | null, code: string, name: string, permissions: { __typename?: 'PipelineTemplatePermissions', delete: boolean }, currentVersion?: { __typename?: 'PipelineTemplateVersion', id: string, versionNumber: number, createdAt: any, user?: { __typename?: 'User', id: string, email: string, displayName: string, avatar: { __typename?: 'Avatar', initials: string, color: string } } | null, template: { __typename?: 'PipelineTemplate', sourcePipeline?: { __typename?: 'Pipeline', name?: string | null } | null } } | null }> } };
+export type GetPipelineTemplatesQuery = { __typename?: 'Query', pipelineTemplates: { __typename?: 'PipelineTemplatePage', pageNumber: number, totalPages: number, totalItems: number, items: Array<{ __typename?: 'PipelineTemplate', id: string, description?: string | null, code: string, name: string, permissions: { __typename?: 'PipelineTemplatePermissions', delete: boolean }, workspace?: { __typename?: 'Workspace', slug: string, name: string } | null, currentVersion?: { __typename?: 'PipelineTemplateVersion', id: string, versionNumber: number, createdAt: any, user?: { __typename?: 'User', id: string, email: string, displayName: string, avatar: { __typename?: 'Avatar', initials: string, color: string } } | null, template: { __typename?: 'PipelineTemplate', sourcePipeline?: { __typename?: 'Pipeline', name?: string | null } | null } } | null }> } };
 
 export type PipelineTemplates_WorkspaceFragment = { __typename?: 'Workspace', slug: string };
 
@@ -39,6 +39,10 @@ export const GetPipelineTemplatesDocument = gql`
       name
       permissions {
         delete
+      }
+      workspace {
+        slug
+        name
       }
       currentVersion {
         id
