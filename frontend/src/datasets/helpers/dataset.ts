3ee54277cc3f4790dc9c2700bf67fc36bcb87931
Quentin Gérôme <quentin@letsbuildit.dev>
2025-06-16 17:19:36 +0200

    feat(Storage): Support Azure Blob storage
    Squashed Quentin's commit for easier rebasing on `main`.
    
    Full details:
    
    chore(docker): Force amd64 platform on docker
    
    refactor!(Settings): Merge dev, local, production config into one "base" file.
    
    - Ease the configuration of the storage backend (and raise an error when missing values)
    - Adapt the docker build  & dockerfile to work with the changes
    
    BREAKING CHANGES
    - Devs have to change `DJANGO_SETTINGS_MODULE` in their .env to new value in .env.dist (`config.settings.base`)
    - Local hosting solutions using docker have to provide `PIPELINE_SCHEDULER_SPAWNER=docker` in the env vars
    - `STORAGE_BACKEND` must be set to the appropriate value in BLSQ deployments (`gcp`)
    
    chore(Storage): Storage backend can require headers to be supplied with the upload/download requests
    Improve (a bit) the signatures of the methods for the storage backend
    
    feat(Storage): Support Azure Blob storage
    Run tests using azurite
    
    fix(DB): Adaptation for PG16
    
    fix: Fix demo data import
    
    fix(AzureBlobStorage): Update placeholder blob naming and improve delete logic
    
    fix: Remove the STORAGE_BACKEND='azure' from the docker-compose.yaml file
    
    chore: Launch azurite when testing the application on GIthub
    
    fix: Update Azurite configuration for testing and adjust connection settings in AzureBlobStorage tests
    
    fix typo

--- frontend/src/datasets/helpers/dataset.ts
+++ frontend/src/datasets/helpers/dataset.ts
@@ -212,6 +212,7 @@ export async function generateDatasetUploadUrl(
           success
           errors
           uploadUrl
+          headers
         }
       }
     `,
@@ -225,7 +226,10 @@ export async function generateDatasetUploadUrl(
   });
 
   if (data?.generateDatasetUploadUrl.success) {
-    return data.generateDatasetUploadUrl.uploadUrl!;
+    return {
+      url: data.generateDatasetUploadUrl.uploadUrl!,
+      headers: data.generateDatasetUploadUrl.headers!,
+    };
   } else {
     throw new Error(
       `An unknown error occurred: ${data?.generateDatasetUploadUrl.errors}`,
