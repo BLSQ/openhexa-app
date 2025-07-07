cdd7446c6bee66d4fc550e181d9b4345d9c1c2f6
Yolan Fery <yolan@codethefuture.be>
2025-07-07 10:12:46 +0200

    fix: filter for user methods to support pipeline token for most methods (HEXA-1316 ) (#1237)

--- backend/hexa/webapps/models.py
+++ backend/hexa/webapps/models.py
@@ -15,6 +15,14 @@ from hexa.workspaces.models import Workspace
 
 class WebappQuerySet(BaseQuerySet, SoftDeleteQuerySet):
     def filter_for_user(self, user: AnonymousUser | User):
+        # FIXME: Use a generic permission system instead of differencing between User and PipelineRunUser
+        from hexa.pipelines.authentication import PipelineRunUser
+
+        if isinstance(user, PipelineRunUser):
+            return self._filter_for_user_and_query_object(
+                user,
+                models.Q(workspace=user.pipeline_run.pipeline.workspace),
+            )
         return self._filter_for_user_and_query_object(
             user,
             Q(workspace__members=user),
