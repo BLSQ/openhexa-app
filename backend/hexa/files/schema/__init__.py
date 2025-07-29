ccb5b91584d4830bf7285f135b5fd6f0a0e59f7d
Bram Jans <ik@bramjans.com>
2025-07-29 12:17:05 +0200

    Add API endpoint to fetch file by its path for workspace

--- backend/hexa/files/schema/__init__.py
+++ backend/hexa/files/schema/__init__.py
@@ -3,10 +3,11 @@ import pathlib
 from ariadne import load_schema_from_path
 
 from .mutations import bindables as mutations_bindables
+from .queries import bindables as queries_bindables
 from .types import bindables as types_bindables
 
 files_type_def = load_schema_from_path(
     f"{pathlib.Path(__file__).parent.parent.resolve()}/graphql/schema.graphql"
 )
 
-files_bindables = types_bindables + mutations_bindables
+files_bindables = types_bindables + mutations_bindables + queries_bindables
---
+++ backend/hexa/files/schema/queries.py
@@ -0,0 +1,35 @@
+from ariadne import QueryType
+from django.core.exceptions import ImproperlyConfigured
+from django.http import HttpRequest
+
+from hexa.files import storage
+from hexa.files.backends.exceptions import NotFound
+from hexa.workspaces.models import Workspace
+
+files_queries = QueryType()
+
+
+@files_queries.field("getFileByPath")
+def resolve_get_file_by_path(_, info, workspace_slug, path):
+    request: HttpRequest = info.context["request"]
+
+    try:
+        workspace = Workspace.objects.filter_for_user(request.user).get(
+            slug=workspace_slug
+        )
+    except Workspace.DoesNotExist:
+        return None
+
+    if workspace.bucket_name is None:
+        raise ImproperlyConfigured("Workspace does not have a bucket")
+
+    if not request.user.has_perm("files.download_object", workspace):
+        return {"success": False, "errors": ["PERMISSION_DENIED"]}
+
+    try:
+        return storage.get_bucket_object(workspace.bucket_name, path)
+    except NotFound:
+        return None
+
+
+bindables = [files_queries]
