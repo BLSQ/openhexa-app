391e4577e7193912b6fb3e575d9abc13e36e022c
Bram Jans <ik@bramjans.com>
2025-07-29 12:17:05 +0200

    Add API endpoint to fetch file by its path for workspace

--- frontend/src/graphql/types.ts
+++ frontend/src/graphql/types.ts
@@ -3886,6 +3886,8 @@ export type Query = {
   datasetVersionFile?: Maybe<DatasetVersionFile>;
   /** Search datasets. */
   datasets: DatasetPage;
+  /** Get a file by its path within a workspace. */
+  getFileByPath?: Maybe<BucketObject>;
   /** Retrieves the currently authenticated user. */
   me: Me;
   metadataAttributes: Array<Maybe<MetadataAttribute>>;
@@ -4051,6 +4053,12 @@ export type QueryDatasetsArgs = {
   page?: InputMaybe<Scalars['Int']['input']>;
   perPage?: InputMaybe<Scalars['Int']['input']>;
   query?: InputMaybe<Scalars['String']['input']>;
+};
+
+
+export type QueryGetFileByPathArgs = {
+  path: Scalars['String']['input'];
+  workspaceSlug: Scalars['String']['input'];
 };
 
 
