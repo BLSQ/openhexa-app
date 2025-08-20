b818b1df3568eadb7e3c7e0c42f2d2662f14aed2
YolanFery <yolan@codethefuture.be>
2025-08-20 11:16:21 +0200

    Revert "Fix lint"
    This reverts commit b461b21faebab4e062ac2a03755d6a58e1159368.

--- frontend/src/datasets/features/DatasetExplorer/fragments.generated.tsx
+++ frontend/src/datasets/features/DatasetExplorer/fragments.generated.tsx
@@ -2,11 +2,9 @@ import * as Types from '../../../graphql/types';
 
 import { gql } from '@apollo/client';
 import { DownloadVersionFile_FileFragmentDoc } from '../DownloadVersionFile/DownloadVersionFile.generated';
-import { DatasetVersionFileSample_FileFragmentDoc, DatasetVersionFileSample_VersionFragmentDoc } from '../DatasetVersionFileSample/DatasetVersionFileSample.generated';
-import { DatasetVersionFileColumns_FileFragmentDoc, DatasetVersionFileColumns_VersionFragmentDoc } from '../DatasetVersionFileColumns/DatasetVersionFileColumns.generated';
+import { DatasetVersionFileSample_FileFragmentDoc } from '../DatasetVersionFileSample/DatasetVersionFileSample.generated';
+import { DatasetVersionFileColumns_FileFragmentDoc } from '../DatasetVersionFileColumns/DatasetVersionFileColumns.generated';
 export type DatasetExplorer_FileFragment = { __typename?: 'DatasetVersionFile', id: string, filename: string, createdAt: any, contentType: string, size: any, uri: string, downloadUrl?: string | null, targetId: any, properties?: any | null, createdBy?: { __typename?: 'User', displayName: string } | null, attributes: Array<{ __typename: 'MetadataAttribute', id: string, key: string, value?: any | null, label?: string | null, system: boolean }> };
-
-export type DatasetExplorer_VersionFragment = { __typename?: 'DatasetVersion', id: string, name: string, files: { __typename?: 'DatasetVersionFilePage', totalPages: number, pageNumber: number, totalItems: number, items: Array<{ __typename?: 'DatasetVersionFile', id: string, filename: string, createdAt: any, contentType: string, size: any, uri: string, downloadUrl?: string | null, targetId: any, properties?: any | null, createdBy?: { __typename?: 'User', displayName: string } | null, attributes: Array<{ __typename: 'MetadataAttribute', id: string, key: string, value?: any | null, label?: string | null, system: boolean }> }> }, dataset: { __typename?: 'Dataset', slug: string, workspace?: { __typename?: 'Workspace', slug: string } | null, permissions: { __typename?: 'DatasetPermissions', update: boolean } } };
 
 export const DatasetExplorer_FileFragmentDoc = gql`
     fragment DatasetExplorer_file on DatasetVersionFile {
@@ -26,20 +24,3 @@ export const DatasetExplorer_FileFragmentDoc = gql`
     ${DownloadVersionFile_FileFragmentDoc}
 ${DatasetVersionFileSample_FileFragmentDoc}
 ${DatasetVersionFileColumns_FileFragmentDoc}`;
-export const DatasetExplorer_VersionFragmentDoc = gql`
-    fragment DatasetExplorer_version on DatasetVersion {
-  id
-  files(page: $page, perPage: $perPage) {
-    totalPages
-    pageNumber
-    totalItems
-    items {
-      ...DatasetExplorer_file
-    }
-  }
-  ...DatasetVersionFileSample_version
-  ...DatasetVersionFileColumns_version
-}
-    ${DatasetExplorer_FileFragmentDoc}
-${DatasetVersionFileSample_VersionFragmentDoc}
-${DatasetVersionFileColumns_VersionFragmentDoc}`;
