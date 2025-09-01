859a89020c6a11f40b87812f0b17d99437f41857
Bram Jans <ik@bramjans.com>
2025-09-01 22:16:50 +0200

    Improve SimplePagination component

--- frontend/src/core/components/Pagination/SimplePagination.tsx
+++ frontend/src/core/components/Pagination/SimplePagination.tsx
@@ -2,22 +2,27 @@ import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 import clsx from "clsx";
 import { useTranslation } from "next-i18next";
 import Spinner from "../Spinner";
+import SimpleSelect from "../forms/SimpleSelect";
 import PaginationItem from "./PaginationItem";
 
 type Props = {
   loading?: boolean;
   page: number;
+  perPage?: number;
+  perPageOptions?: number[];
   hasNextPage: boolean;
   hasPreviousPage: boolean;
   className?: string;
   children?: React.ReactNode;
-  onChange(page: number): void;
+  onChange(page: number, perPage?: number): void;
 };
 
 const SimplePagination = (props: Props) => {
   const {
     loading,
     page,
+    perPage,
+    perPageOptions,
     onChange,
     className,
     hasNextPage,
@@ -31,12 +36,12 @@ const SimplePagination = (props: Props) => {
     <div className={clsx("flex items-center justify-between py-3", className)}>
       <div className="flex flex-1 justify-between sm:hidden">
         {hasPreviousPage && (
-          <PaginationItem onClick={() => onChange(page - 1)}>
+          <PaginationItem onClick={() => onChange(page - 1, perPage)}>
             {t("Previous")}
           </PaginationItem>
         )}
         {hasNextPage && (
-          <PaginationItem onClick={() => onChange(page + 1)}>
+          <PaginationItem onClick={() => onChange(page + 1, perPage)}>
             {t("Next")}
           </PaginationItem>
         )}
@@ -55,12 +60,28 @@ const SimplePagination = (props: Props) => {
         )}
         {!loading && children}
         <div className="flex items-center space-x-2">
+          {perPageOptions && perPage && (
+            <SimpleSelect
+              required
+              onChange={(event) =>
+                onChange(1, parseInt(event.currentTarget.value, 10))
+              }
+              className="h-[30px] w-fit py-0"
+              value={perPage.toString()}
+            >
+              {perPageOptions.map((opt) => (
+                <option key={opt} value={opt.toString()}>
+                  {opt}
+                </option>
+              ))}
+            </SimpleSelect>
+          )}
           <nav
             className="relative z-0 inline-flex -space-x-px rounded-md shadow-xs"
             aria-label="Pagination"
           >
             <PaginationItem
-              onClick={() => onChange(page - 1)}
+              onClick={() => onChange(page - 1, perPage)}
               disabled={!hasPreviousPage}
             >
               <span className="sr-only">{t("Previous")}</span>
@@ -68,7 +89,7 @@ const SimplePagination = (props: Props) => {
             </PaginationItem>
 
             <PaginationItem
-              onClick={() => onChange(page + 1)}
+              onClick={() => onChange(page + 1, perPage)}
               disabled={!hasNextPage}
             >
               <span className="sr-only">{t("Next")}</span>
--- frontend/src/workspaces/components/FileSystemDataGrid/FileSystemDataGrid.tsx
+++ frontend/src/workspaces/components/FileSystemDataGrid/FileSystemDataGrid.tsx
@@ -1,8 +1,8 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import { useTranslation } from "next-i18next";
 import clsx from "clsx";
 
-import { FolderIcon, DocumentIcon } from "@heroicons/react/24/outline";
+import { FolderIcon } from "@heroicons/react/24/outline";
 import { BucketObject, BucketObjectType } from "graphql/types";
 
 import DataGrid, { BaseColumn } from "core/components/DataGrid";
@@ -11,7 +11,6 @@ import Filesize from "core/components/Filesize";
 import Link from "core/components/Link";
 import SimplePagination from "core/components/Pagination/SimplePagination";
 import Pagination from "core/components/Pagination/Pagination";
-import SimpleSelect from "core/components/forms/SimpleSelect";
 import DropzoneOverlay from "core/components/DropzoneOverlay";
 
 import { getFileIconAndColor } from "../../features/FileBrowserDialog/utils";
@@ -68,10 +67,6 @@ const FileSystemDataGrid: React.FC<FileSystemDataGridProps> = ({
   showPageSizeSelect = true,
 }) => {
   const { t } = useTranslation();
-
-  const formatDate = (date: string) => {
-    return new Date(date).toLocaleDateString();
-  };
 
   const renderNameColumn = (item: BucketObject) => {
     const isDirectory = item.type === BucketObjectType.Directory;
@@ -155,30 +150,7 @@ const FileSystemDataGrid: React.FC<FileSystemDataGridProps> = ({
     <>
       {dataGridContent}
       {data.length > 0 && (
-        <div className="px-4 flex justify-between items-center">
-          <div>
-            {showPageSizeSelect && (
-              <SimpleSelect
-                required
-                onChange={(event) =>
-                  onChangePage(
-                    1,
-                    event.target.value
-                      ? parseInt(event.target.value, 10)
-                      : perPage,
-                  )
-                }
-                className="h-[30px] w-fit py-0"
-                value={perPage.toString()}
-              >
-                {[10, 20, 50, 100].map((opt) => (
-                  <option key={opt} value={opt.toString()}>
-                    {opt}
-                  </option>
-                ))}
-              </SimpleSelect>
-            )}
-          </div>
+        <div className="px-4">
           {isFullPagination(pagination) ? (
             <Pagination
               page={pagination.pageNumber}
@@ -197,7 +169,11 @@ const FileSystemDataGrid: React.FC<FileSystemDataGridProps> = ({
               hasNextPage={pagination.hasNextPage}
               hasPreviousPage={pagination.hasPreviousPage}
               page={pagination.pageNumber}
-              onChange={(page) => onChangePage(page, perPage)}
+              perPage={perPage}
+              perPageOptions={
+                showPageSizeSelect ? [10, 20, 50, 100] : undefined
+              }
+              onChange={onChangePage}
             />
           )}
         </div>
