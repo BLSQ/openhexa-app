60a38a78560e4471ebcd4696a67ca45b76aed005
YolanFery <yolan@codethefuture.be>
2025-09-18 16:39:31 +0200

    fix

--- backend/hexa/user_management/permissions.py
+++ backend/hexa/user_management/permissions.py
@@ -55,10 +55,10 @@ def manage_members(principal: User, organization: Organization):
 
 def create_workspace(principal: User, organization: Organization):
     """Only admin and owner users can create a workspace"""
-    return (principal.is_organization_admin_or_owner(
-        organization
-
-    ) or principal.has_feature_flag("workspaces.create")) and not principal.has_feature_flag("workspaces.prevent_create")
+    return (
+        principal.is_organization_admin_or_owner(organization)
+        or principal.has_feature_flag("workspaces.create")
+    ) and not principal.has_feature_flag("workspaces.prevent_create")
 
 
 def archive_workspace(principal: User, organization: Organization):
