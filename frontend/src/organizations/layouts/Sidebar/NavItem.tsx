71c19e36f84c7193c3cc601a9d041673c5ac865b
YolanFery <yolan@codethefuture.be>
2025-09-23 08:31:29 +0200

    Uniform size icon

--- frontend/src/organizations/layouts/Sidebar/NavItem.tsx
+++ frontend/src/organizations/layouts/Sidebar/NavItem.tsx
@@ -23,7 +23,14 @@ const NavItem = ({ label, href, Icon, compact, className }: NavItemProps) => {
         compact && "justify-center",
       )}
     >
-      {Icon && <Icon className={clsx(compact ? "h-7 w-7" : "ml-1 h-5 w-5")} />}
+      {Icon && (
+        <Icon
+          className={clsx(
+            compact ? "h-7 w-7" : "ml-1 h-5 w-5",
+            "flex-shrink-0",
+          )}
+        />
+      )}
       {compact ? (
         <div className="absolute inset-y-0 left-full ml-1.5 hidden h-full items-center text-xs opacity-0 transition-opacity group-hover:flex group-hover:opacity-100">
           <Badge className="bg-gray-800 ring-gray-500/20">{label}</Badge>
