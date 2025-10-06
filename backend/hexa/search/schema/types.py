632a3719cb0318007ba0a773e451a7875c27ff6f
Bram Jans <ik@bramjans.com>
2025-10-06 15:27:44 +0200

    simplify, use alias + translation

--- backend/hexa/search/schema/types.py
+++ backend/hexa/search/schema/types.py
@@ -2,11 +2,7 @@ from ariadne import ObjectType
 
 file_type = ObjectType("File")
 
-
-@file_type.field("updated")
-def resolve_updated(obj, info):
-    """Deprecated field, maps to updated_at for backwards compatibility."""
-    return getattr(obj, "updated_at", None)
-
+# Deprecated field, maps to updated_at for backwards compatibility.
+file_type.set_alias("updated", "updated_at")
 
 bindables = [file_type]
