77dec119529622ba60bdfaaaf9677e4e8865505d
YolanFery <yolan@codethefuture.be>
2025-10-10 14:22:49 +0200

    Not needed

--- backend/hexa/datasets/schema/types.py
+++ backend/hexa/datasets/schema/types.py
@@ -129,11 +129,6 @@ def resolve_dataset_updated_at(obj: Dataset, info, **kwargs):
     )
 
 
-@dataset_link_object.field("workspace")
-def resolve_dataset_link_workspace(obj: DatasetLink, info, **kwargs):
-    return obj.workspace
-
-
 @dataset_link_object.field("permissions")
 def resolve_dataset_link_permissions(obj: DatasetLink, info, **kwargs):
     return obj
