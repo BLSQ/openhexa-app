6204cbe5be04a6383a77054ae4e78174629be3c2
YolanFery <yolan@codethefuture.be>
2025-10-10 10:04:16 +0200

    Config

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -1,8 +1,12 @@
+import os
+
 from .base import *  # noqa: F403, F401
 
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 SECURE_SSL_REDIRECT = False
+
+ENABLE_SQL_LOGGING = os.environ.get("IS_LOCAL_DEV", "0") == "1"
 
 LOGGING = {
     "version": 1,
@@ -20,15 +24,17 @@ LOGGING = {
         },
     },
     "loggers": {
-        "django.db.backends": {
-            "level": "DEBUG",
-            "handlers": ["console"],
-        },
         "hexa.core.middlewares": {
             "level": "WARNING",
             "handlers": ["console"],
         },
     },
 }
+
+if ENABLE_SQL_LOGGING:
+    LOGGING["loggers"]["django.db.backends"] = {
+        "level": "DEBUG",
+        "handlers": ["console"],
+    }
 
 MIDDLEWARE += ["hexa.core.middlewares.query_count_debug_middleware"]  # noqa: F405
