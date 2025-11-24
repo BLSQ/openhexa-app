08886965874cb25ae0a53d2fa662925c194b0794
nazarfil <nfilipchuk@bluesquarehub.com>
2025-11-24 18:43:32 +0100

    feat: skip run if pipeline is already running for scheduld pipelines

--- backend/hexa/pipelines/management/commands/pipelines_scheduler.py
+++ backend/hexa/pipelines/management/commands/pipelines_scheduler.py
@@ -7,7 +7,12 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 
 from hexa.analytics.api import track
-from hexa.pipelines.models import Pipeline, PipelineRunTrigger
+from hexa.pipelines.models import (
+    Pipeline,
+    PipelineRun,
+    PipelineRunState,
+    PipelineRunTrigger,
+)
 
 logger = getLogger(__name__)
 
@@ -47,6 +52,16 @@ class Command(BaseCommand):
                 if real_delay > 0:
                     logger.debug(f"sleep before run: {real_delay}")
                     sleep(real_delay)
+
+                if PipelineRun.objects.filter(
+                    pipeline=pipeline,
+                    state__in=[PipelineRunState.QUEUED, PipelineRunState.RUNNING],
+                ).exists():
+                    logger.warning(
+                        "pipeline %s already has a run in progress, skipping scheduled execution",
+                        pipeline.id,
+                    )
+                    continue
 
                 pipeline.run(
                     user=None,
