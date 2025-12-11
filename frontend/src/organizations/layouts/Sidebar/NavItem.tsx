d3926190c442e96ffe9db1f35e309bee6343b766
YolanFery <yolan@codethefuture.be>
2025-12-11 12:28:06 +0100

    UI consistency

--- frontend/src/organizations/layouts/Sidebar/NavItem.tsx
+++ frontend/src/organizations/layouts/Sidebar/NavItem.tsx
@@ -41,33 +41,38 @@ const NavItem = ({
       className={clsx(
         className,
         "text-md group relative flex items-center gap-3 px-2 py-2 font-medium",
-        "text-gray-300 hover:bg-gray-700 hover:text-white",
+        isCurrent
+          ? "text-white"
+          : "text-gray-300 hover:bg-gray-700 hover:text-white",
         compact && "justify-center",
       )}
     >
-      {isCurrent && (
-        <div
-          className={clsx(
-            "absolute inset-y-0 left-0 w-1 bg-pink-500 transition-opacity",
-            isCurrent ? "opacity-100" : "opacity-0",
-          )}
-        ></div>
-      )}
+      <div
+        className={clsx(
+          "absolute inset-y-0 left-0 w-1 bg-pink-500 transition-opacity",
+          isCurrent ? "opacity-100" : "opacity-0",
+        )}
+      ></div>
       {logo ? (
         <img
           src={logo}
           alt={label || ""}
-          className="h-7 w-7 object-contain flex-shrink-0"
+          className={clsx(
+            "object-contain flex-shrink-0",
+            compact ? "h-7 w-7" : "ml-1 h-5 w-5",
+          )}
         />
       ) : Icon ? (
-        <Icon className={clsx("h-7 w-7 flex-shrink-0", !compact && "ml-1")} />
+        <Icon className={clsx(compact ? "h-7 w-7" : "ml-1 h-5 w-5")} />
       ) : null}
       {compact ? (
-        <div className="absolute inset-y-0 left-full ml-2.5 hidden h-full items-center text-xs opacity-0 transition-opacity group-hover:flex group-hover:opacity-100">
+        <div className="absolute inset-y-0 left-full ml-1.5 hidden h-full items-center text-xs opacity-0 transition-opacity group-hover:flex group-hover:opacity-100">
           <Badge className="bg-gray-800 ring-gray-500/20">{label}</Badge>
         </div>
       ) : (
-        <span>{delayedLabel}</span>
+        <span className="whitespace-nowrap overflow-hidden transition-opacity duration-200">
+          {delayedLabel}
+        </span>
       )}
     </Link>
   );
