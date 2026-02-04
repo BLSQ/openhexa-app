314dba5bccedea05f265295e9b2f22a675fbd500
DimitriKwihangana <kwihanganadimitri@gmail.com>
2026-02-04 20:22:34 +0200

    fix: Redirecting pipelines with dataset type runs to particular versions

--- frontend/src/workspaces/features/RunOutputsTable/RunOutputsTable.generated.tsx
+++ frontend/src/workspaces/features/RunOutputsTable/RunOutputsTable.generated.tsx
@@ -4,7 +4,7 @@ import { gql } from '@apollo/client';
 import { DownloadBucketObject_WorkspaceFragmentDoc } from '../DownloadBucketObject/DownloadBucketObject.generated';
 export type RunOutputsTable_WorkspaceFragment = { __typename?: 'Workspace', slug: string, bucket: { __typename?: 'Bucket', name: string } };
 
-export type RunOutputsTable_RunFragment = { __typename?: 'PipelineRun', id: string, outputs: Array<{ __typename: 'BucketObject', name: string, key: string, path: string, type: Types.BucketObjectType } | { __typename: 'DatabaseTable', tableName: string } | { __typename: 'GenericOutput', genericName?: string | null, genericType: string, genericUri: string }>, datasetVersions: Array<{ __typename?: 'DatasetVersion', name: string, dataset: { __typename?: 'Dataset', slug: string, name: string, workspace?: { __typename?: 'Workspace', slug: string } | null } }> };
+export type RunOutputsTable_RunFragment = { __typename?: 'PipelineRun', id: string, outputs: Array<{ __typename: 'BucketObject', name: string, key: string, path: string, type: Types.BucketObjectType } | { __typename: 'DatabaseTable', tableName: string } | { __typename: 'GenericOutput', genericName?: string | null, genericType: string, genericUri: string }>, datasetVersions: Array<{ __typename?: 'DatasetVersion', id: string, name: string, dataset: { __typename?: 'Dataset', slug: string, name: string, workspace?: { __typename?: 'Workspace', slug: string } | null } }> };
 
 export const RunOutputsTable_WorkspaceFragmentDoc = gql`
     fragment RunOutputsTable_workspace on Workspace {
@@ -36,6 +36,7 @@ export const RunOutputsTable_RunFragmentDoc = gql`
     }
   }
   datasetVersions {
+    id
     name
     dataset {
       slug
