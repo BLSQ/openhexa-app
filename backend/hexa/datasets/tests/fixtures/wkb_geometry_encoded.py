c7a5cd19add1c058e5a734ad77a46525b4df893d
mrivar <martin7557@gmail.com>
2026-02-09 17:12:11 +0100

    fix tests

--- backend/hexa/datasets/tests/fixtures/wkb_geometry_encoded.py
+++ backend/hexa/datasets/tests/fixtures/wkb_geometry_encoded.py
@@ -1,14 +1,14 @@
 wkb_geometry = {
     "bbox": {
         "xmax": 40.31659000000002,
-        "xmin": 29.33999759290,
-        "ymax": 71.35776357694175,
-        "ymin": 18.91619,
+        "xmin": 29.339997592900346,
+        "ymax": -0.9500000000000001,
+        "ymin": -11.720938002166735,
     },
     "geometry": "<SKIPPED_BYTES>",
-    "name": "United States of America",
-    "iso_a3": "USA",
-    "pop_est": 328239523.0,
-    "continent": "North America",
-    "gdp_md_est": 21433226,
+    "name": "Tanzania",
+    "iso_a3": "TZA",
+    "pop_est": 58005463.0,
+    "continent": "Africa",
+    "gdp_md_est": 63177,
 }
