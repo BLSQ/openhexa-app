9172641854bf356904820b0b46c58ca3ba99d0d5
YolanFery <yolan@codethefuture.be>
2026-02-10 17:19:38 +0100

    feat: model

---
+++ backend/hexa/pipelines/migrations/0064_pipelinerun_resource_limits.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("pipelines", "0063_alter_pipelinerun_state"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="pipelinerun",
+            name="cpu_limit",
+            field=models.CharField(
+                max_length=32,
+                default=settings.PIPELINE_DEFAULT_CONTAINER_CPU_LIMIT,
+            ),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="pipelinerun",
+            name="memory_limit",
+            field=models.CharField(
+                max_length=32,
+                default=settings.PIPELINE_DEFAULT_CONTAINER_MEMORY_LIMIT,
+            ),
+            preserve_default=False,
+        ),
+    ]
