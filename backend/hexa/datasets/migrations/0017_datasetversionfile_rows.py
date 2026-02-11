30b46ba3c925d5227be67df8e99c20cd94d91efa
mrivar <martin7557@gmail.com>
2026-02-11 17:25:37 +0100

    make each file modification atomic, in order to avoid a full migration rerun on errors

--- backend/hexa/datasets/migrations/0017_datasetversionfile_rows.py
+++ backend/hexa/datasets/migrations/0017_datasetversionfile_rows.py
@@ -4,47 +4,40 @@ from django.db import migrations, models, transaction
 
 def update_dataset_samples_and_rows(apps, schema_editor):
     DatasetVersionFile = apps.get_model("datasets", "DatasetVersionFile")
-    DatasetFileSample = apps.get_model("datasets", "DatasetFileSample")
 
     from django.conf import settings
 
     from hexa.datasets.queue import load_df
 
-    queryset = DatasetVersionFile.objects.prefetch_related("samples")
-
-    files_to_update = []
-    samples_to_update = []
+    queryset = DatasetVersionFile.objects.filter(rows__isnull=True).prefetch_related(
+        "samples"
+    )
 
     for version_file in queryset.iterator(chunk_size=1000):
-        version_file.filename = version_file.uri.split("/")[-1] # cannot read properties using apps.get_model
-        df = load_df(version_file)
-        version_file.rows = len(df.index)
-        files_to_update.append(version_file)
+        with transaction.atomic():
+            version_file.filename = version_file.uri.split("/")[
+                -1
+            ]  # cannot read properties using apps.get_model
+            df = load_df(version_file)
+            version_file.rows = len(df.index)
+            version_file.save(update_fields=["rows"])
 
-        for version_sample in version_file.samples.all():
-            if not df.empty:
+            if not df.empty and len(df.index) < 50:
                 sample = df.head(settings.WORKSPACE_DATASETS_FILE_SNAPSHOT_SIZE)
                 sample = sample.replace({np.inf: "inf", -np.inf: "-inf"})
-                version_sample.sample = sample.to_dict(orient="records")
-            samples_to_update.append(version_sample)
-
-    with transaction.atomic():
-        if files_to_update:
-            DatasetVersionFile.objects.bulk_update(
-                files_to_update,
-                ["rows"],
-                batch_size=1000,
-            )
-
-        if samples_to_update:
-            DatasetFileSample.objects.bulk_update(
-                samples_to_update,
-                ["sample"],
-                batch_size=1000,
-            )
+                sample = sample.to_dict(orient="records")
+                for version_sample in version_file.samples.all():
+                    version_sample.sample = sample
+                    version_sample.save(update_fields=["sample"])
 
 
 class Migration(migrations.Migration):
+    # This migration might take minutes in heavy-filled environments:
+    # Setting atomic to false prevents a giant DB transaction, avoiding long DB locks
+    # Risk: if there is an error, db will be partially migrated, and migration will have to be launched again
+    # (however, in this case, we are already filtering correctly migrated files with `.filter(rows__isnull=True)`)
+    atomic = False
+
     dependencies = [
         ("datasets", "0016_add_shared_with_organization"),
     ]
@@ -55,5 +48,7 @@ class Migration(migrations.Migration):
             name="rows",
             field=models.IntegerField(default=None, null=True),
         ),
-        migrations.RunPython(update_dataset_samples_and_rows, migrations.RunPython.noop),
+        migrations.RunPython(
+            update_dataset_samples_and_rows, migrations.RunPython.noop
+        ),
     ]
