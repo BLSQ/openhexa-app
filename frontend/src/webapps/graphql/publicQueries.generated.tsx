a1a46f67350e39b3a2569bb124836272d5d186cc
YolanFery <yolan@codethefuture.be>
2026-02-20 17:04:53 +0100

    fix: queries

--- frontend/src/webapps/graphql/publicQueries.generated.tsx
+++ frontend/src/webapps/graphql/publicQueries.generated.tsx
@@ -3,58 +3,57 @@ import * as Types from '../../graphql/types';
 import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
 const defaultOptions = {} as const;
-export type PublicWebappQueryVariables = Types.Exact<{
+export type WebappPlayQueryVariables = Types.Exact<{
   workspaceSlug: Types.Scalars['String']['input'];
   webappSlug: Types.Scalars['String']['input'];
 }>;
 
 
-export type PublicWebappQuery = { __typename?: 'Query', publicWebapp?: { __typename?: 'Webapp', id: string, slug: string, name: string, url: string, type: Types.WebappType, isPublic: boolean } | null };
+export type WebappPlayQuery = { __typename?: 'Query', webapp?: { __typename?: 'Webapp', id: string, slug: string, name: string, url: string, type: Types.WebappType } | null };
 
 
-export const PublicWebappDocument = gql`
-    query PublicWebapp($workspaceSlug: String!, $webappSlug: String!) {
-  publicWebapp(workspaceSlug: $workspaceSlug, slug: $webappSlug) {
+export const WebappPlayDocument = gql`
+    query WebappPlay($workspaceSlug: String!, $webappSlug: String!) {
+  webapp(workspaceSlug: $workspaceSlug, slug: $webappSlug) {
     id
     slug
     name
     url
     type
-    isPublic
   }
 }
     `;
 
 /**
- * __usePublicWebappQuery__
+ * __useWebappPlayQuery__
  *
- * To run a query within a React component, call `usePublicWebappQuery` and pass it any options that fit your needs.
- * When your component renders, `usePublicWebappQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * To run a query within a React component, call `useWebappPlayQuery` and pass it any options that fit your needs.
+ * When your component renders, `useWebappPlayQuery` returns an object from Apollo Client that contains loading, error, and data properties
  * you can use to render your UI.
  *
  * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
  *
  * @example
- * const { data, loading, error } = usePublicWebappQuery({
+ * const { data, loading, error } = useWebappPlayQuery({
  *   variables: {
  *      workspaceSlug: // value for 'workspaceSlug'
  *      webappSlug: // value for 'webappSlug'
  *   },
  * });
  */
-export function usePublicWebappQuery(baseOptions: Apollo.QueryHookOptions<PublicWebappQuery, PublicWebappQueryVariables> & ({ variables: PublicWebappQueryVariables; skip?: boolean; } | { skip: boolean; }) ) {
+export function useWebappPlayQuery(baseOptions: Apollo.QueryHookOptions<WebappPlayQuery, WebappPlayQueryVariables> & ({ variables: WebappPlayQueryVariables; skip?: boolean; } | { skip: boolean; }) ) {
         const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<PublicWebappQuery, PublicWebappQueryVariables>(PublicWebappDocument, options);
+        return Apollo.useQuery<WebappPlayQuery, WebappPlayQueryVariables>(WebappPlayDocument, options);
       }
-export function usePublicWebappLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<PublicWebappQuery, PublicWebappQueryVariables>) {
+export function useWebappPlayLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<WebappPlayQuery, WebappPlayQueryVariables>) {
           const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<PublicWebappQuery, PublicWebappQueryVariables>(PublicWebappDocument, options);
+          return Apollo.useLazyQuery<WebappPlayQuery, WebappPlayQueryVariables>(WebappPlayDocument, options);
         }
-export function usePublicWebappSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<PublicWebappQuery, PublicWebappQueryVariables>) {
+export function useWebappPlaySuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<WebappPlayQuery, WebappPlayQueryVariables>) {
           const options = baseOptions === Apollo.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
-          return Apollo.useSuspenseQuery<PublicWebappQuery, PublicWebappQueryVariables>(PublicWebappDocument, options);
+          return Apollo.useSuspenseQuery<WebappPlayQuery, WebappPlayQueryVariables>(WebappPlayDocument, options);
         }
-export type PublicWebappQueryHookResult = ReturnType<typeof usePublicWebappQuery>;
-export type PublicWebappLazyQueryHookResult = ReturnType<typeof usePublicWebappLazyQuery>;
-export type PublicWebappSuspenseQueryHookResult = ReturnType<typeof usePublicWebappSuspenseQuery>;
-export type PublicWebappQueryResult = Apollo.QueryResult<PublicWebappQuery, PublicWebappQueryVariables>;
+export type WebappPlayQueryHookResult = ReturnType<typeof useWebappPlayQuery>;
+export type WebappPlayLazyQueryHookResult = ReturnType<typeof useWebappPlayLazyQuery>;
+export type WebappPlaySuspenseQueryHookResult = ReturnType<typeof useWebappPlaySuspenseQuery>;
+export type WebappPlayQueryResult = Apollo.QueryResult<WebappPlayQuery, WebappPlayQueryVariables>;
