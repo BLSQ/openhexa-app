4227abc4cea2ec399ba59d5418b57eacc2797229
YolanFery <yolan@codethefuture.be>
2026-02-20 16:20:48 +0100

    feat: mutation

--- backend/hexa/webapps/schema/mutations.py
+++ backend/hexa/webapps/schema/mutations.py
@@ -124,6 +124,8 @@ def resolve_update_webapp(_, info, **kwargs):
         webapp.description = input["description"]
     if "icon" in input:
         webapp.icon = decode_base64_image(input["icon"]) if input["icon"] else None
+    if "is_public" in input:
+        webapp.is_public = input["is_public"]
 
     try:
         webapp.save()
