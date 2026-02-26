a6892d942b86042eae804a0574b30e2775348ff5
Bram Jans <ik@bramjans.com>
2026-02-26 17:09:46 +0100

    chore: Allow serving OH on paths instead of subdomains (#1649)
    This introduces two optional ENV variables:
    
    ```.env
    NEXT_PUBLIC_BASE_PATH=/openhexa
    NEXT_PUBLIC_API_BASE_PATH=/api
    ```
    
    When set, nginx/express will serve the frontend on `/openhexa` and the backend on
    `/api`. This means that for example the GraphQL endpoint will become
    available at `/api/graphql/`.

--- frontend/server/index.mjs
+++ frontend/server/index.mjs
@@ -4,10 +4,22 @@ import proxy from "express-http-proxy";
 const port = process.env.PORT ?? 3000;
 const dev = process.env.NODE_ENV !== "production";
 const api_url = process.env.OPENHEXA_BACKEND_URL ?? "localhost:8000";
+const api_base_path = process.env.NEXT_PUBLIC_API_BASE_PATH ?? "";
 const max_request_body_size = process.env.MAX_REQUEST_BODY_SIZE;
 
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+const API_PATHS = [
+  "/graphql/",
+  "/auth/logout",
+  "/static/",
+  "/admin/",
+  "/analytics/track/",
+  "/files/up",
+  "/files/dl",
+  "/superset",
+];
 
 app.prepare().then(async () => {
   const server = express();
@@ -21,18 +33,27 @@ app.prepare().then(async () => {
   server.use(
     "/",
     proxy(api_url, {
+      // Decides whether to proxy a request to the backend.
+      // When an API base path prefix is configured (e.g. /api),
+      // we strip it before sending it.
       filter: function (req) {
-        const needsProxy = [
-          "/graphql/",
-          "/auth/logout",
-          "/static/",
-          "/admin/",
-          "/analytics/track/",
-          "/files/up",
-          "/files/dl",
-          "/superset",
-        ].some((path) => req.path.startsWith(path));
-        return needsProxy;
+        const hasPrefix =
+          api_base_path && req.path.startsWith(api_base_path + "/");
+        const realPath = hasPrefix
+          ? req.path.replace(api_base_path, "") || "/"
+          : req.path;
+
+        return API_PATHS.some((path) => realPath.startsWith(path));
+      },
+      // Decides what path to send to the backend.
+      // when a prefix is configured, we again strip it before sending it.
+      proxyReqPathResolver: function (req) {
+        // Use req.originalUrl (not req.path) to preserve query strings.
+        if (api_base_path && req.path.startsWith(api_base_path + "/")) {
+          return req.originalUrl.replace(api_base_path, "") || "/";
+        }
+
+        return req.originalUrl;
       },
       limit: max_request_body_size,
     }),
