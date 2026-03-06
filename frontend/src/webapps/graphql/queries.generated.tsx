16a913576da118c32b7bd6c5d650ec499b091996
YolanFery <yolan@codethefuture.be>
2026-03-06 08:44:23 +0100

    fix: mandatory url

--- frontend/src/webapps/graphql/queries.generated.tsx
+++ frontend/src/webapps/graphql/queries.generated.tsx
@@ -12,7 +12,7 @@ export type SupersetInstancesQueryVariables = Types.Exact<{
 
 export type SupersetInstancesQuery = { __typename?: 'Query', supersetInstances: Array<{ __typename?: 'SupersetInstance', id: string, name: string, url: string }> };
 
-export type WebappPlay_WebappFragment = { __typename?: 'Webapp', slug: string, url?: string | null, name: string, type: Types.WebappType };
+export type WebappPlay_WebappFragment = { __typename?: 'Webapp', slug: string, url: string, name: string, type: Types.WebappType };
 
 export type WebappAccessQueryVariables = Types.Exact<{
   workspaceSlug: Types.Scalars['String']['input'];
@@ -20,7 +20,7 @@ export type WebappAccessQueryVariables = Types.Exact<{
 }>;
 
 
-export type WebappAccessQuery = { __typename?: 'Query', webapp?: { __typename?: 'Webapp', slug: string, url?: string | null, name: string, type: Types.WebappType, workspace: { __typename?: 'Workspace', slug: string, name: string, permissions: { __typename?: 'WorkspacePermissions', manageMembers: boolean, update: boolean, launchNotebookServer: boolean }, shortcuts: Array<{ __typename?: 'ShortcutItem', id: string, name: string, url: string, order: number }>, countries: Array<{ __typename?: 'Country', flag: string, code: string }>, organization?: { __typename?: 'Organization', id: string, name: string, shortName?: string | null, logo?: string | null, permissions: { __typename?: 'OrganizationPermissions', createWorkspace: { __typename?: 'CreateWorkspacePermission', isAllowed: boolean } } } | null } } | null };
+export type WebappAccessQuery = { __typename?: 'Query', webapp?: { __typename?: 'Webapp', slug: string, url: string, name: string, type: Types.WebappType, workspace: { __typename?: 'Workspace', slug: string, name: string, permissions: { __typename?: 'WorkspacePermissions', manageMembers: boolean, update: boolean, launchNotebookServer: boolean }, shortcuts: Array<{ __typename?: 'ShortcutItem', id: string, name: string, url: string, order: number }>, countries: Array<{ __typename?: 'Country', flag: string, code: string }>, organization?: { __typename?: 'Organization', id: string, name: string, shortName?: string | null, logo?: string | null, permissions: { __typename?: 'OrganizationPermissions', createWorkspace: { __typename?: 'CreateWorkspacePermission', isAllowed: boolean } } } | null } } | null };
 
 export type WebappVersion_VersionFragment = { __typename?: 'WebappVersion', id: string, message: string, authorName: string, authorEmail: string, date: any };
 
