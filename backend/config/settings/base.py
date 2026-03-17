c98bf30d5cf32799c4fe13143908e54f23bf5a78
DimitriKwihangana <kwihanganadimitri@gmail.com>
2026-03-17 21:42:37 +0200

    remove wsgi

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -293,7 +293,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "config.wsgi.application"
+ASGI_APPLICATION = "config.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
