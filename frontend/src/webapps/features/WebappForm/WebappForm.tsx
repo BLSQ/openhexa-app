ccb3304726ec812d9f05ff5ed738eda86aa319bb
YolanFery <yolan@codethefuture.be>
2026-03-17 08:46:28 +0100

    fix: feature flag

--- frontend/src/webapps/features/WebappForm/WebappForm.tsx
+++ frontend/src/webapps/features/WebappForm/WebappForm.tsx
@@ -30,6 +30,7 @@ import {
   WebappType,
 } from "graphql/types";
 import { getWebappTypeLabel } from "webapps/helpers";
+import useFeature from "identity/hooks/useFeature";
 import { DEFAULT_HTML_TEMPLATE } from "webapps/helpers/templates";
 
 const DEFAULT_BLUESQUARE_SUPERSET_URL = "https://superset.bluesquare.org";
@@ -57,6 +58,7 @@ type WebappFormProps = {
 
 const WebappForm = ({ workspace, webapp }: WebappFormProps) => {
   const { t } = useTranslation();
+  const [isStaticWebappsEnabled] = useFeature("static_webapps");
   const router = useRouter();
   const [createWebapp] = useCreateWebappMutation();
   const [updateWebapp] = useUpdateWebappMutation();
@@ -219,7 +221,7 @@ const WebappForm = ({ workspace, webapp }: WebappFormProps) => {
             defaultValue={WebappType.Iframe}
             options={[
               WebappType.Iframe,
-              WebappType.Static,
+              ...(isStaticWebappsEnabled ? [WebappType.Static] : []),
               ...(supersetInstances.length > 0 ? [WebappType.Superset] : []),
             ]}
             getOptionLabel={getWebappTypeLabel}
