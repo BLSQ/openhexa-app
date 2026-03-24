294361d11d24309c4326944597ad4d2f60ded95e
YolanFery <yolan@codethefuture.be>
2026-03-24 10:24:41 +0100

    fix: type wiring

--- frontend/src/pages/workspaces/[workspaceSlug]/webapps/[webappSlug]/play/index.tsx
+++ frontend/src/pages/workspaces/[workspaceSlug]/webapps/[webappSlug]/play/index.tsx
@@ -25,6 +25,7 @@ const WorkspaceWebappPlayPage: NextPageWithLayout = (props: Props) => {
       <Page title={webapp.name}>
         <WebappIframe
           url={webapp.url}
+          type={webapp.type}
           style={{ height: "100vh" }}
           showPoweredBy={webapp.type !== WebappType.Superset} // Already a banner in the Superset iframe from the backend
         />
@@ -58,7 +59,7 @@ const WorkspaceWebappPlayPage: NextPageWithLayout = (props: Props) => {
         }
       >
         <WorkspaceLayout.PageContent>
-          <WebappIframe url={webapp.url} />
+          <WebappIframe url={webapp.url} type={webapp.type} />
         </WorkspaceLayout.PageContent>
       </WorkspaceLayout>
     </Page>
