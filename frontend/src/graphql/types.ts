34f85b4b99c63c3805bf06112863ca1a75980874
YolanFery <yolan@codethefuture.be>
2026-04-07 10:25:59 +0200

    fix: schema previewUrl

--- frontend/src/graphql/types.ts
+++ frontend/src/graphql/types.ts
@@ -6165,6 +6165,7 @@ export type Webapp = {
   isShortcut: Scalars['Boolean']['output'];
   name: Scalars['String']['output'];
   permissions: WebappPermissions;
+  previewUrl: Scalars['String']['output'];
   serveUrl: Scalars['String']['output'];
   showPoweredBy: Scalars['Boolean']['output'];
   slug: Scalars['String']['output'];
