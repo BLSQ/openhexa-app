e8fe2a94f85c65d33a875d66f095bb1156365a2d
YolanFery <yolan@codethefuture.be>
2026-04-10 11:13:45 +0200

    fix: redirection

--- backend/hexa/webapps/apps.py
+++ backend/hexa/webapps/apps.py
@@ -5,4 +5,4 @@ class WebappsConfig(CoreAppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "hexa.webapps"
     label = "webapps"
-    ANONYMOUS_URLS = []
+    ANONYMOUS_URLS = ["webapps:auth_token"]
