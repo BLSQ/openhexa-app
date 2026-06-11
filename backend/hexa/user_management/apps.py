dc57eaba24f07799e23d8cc0ee5e8acb3792cc6b
Bram Jans <ik@bramjans.com>
2026-06-11 10:02:58 +0200

    Unregister certain allauth models from Django admin
    They are not used and harmless, but let's hide them to avoid confusion
    in the future.

--- backend/hexa/user_management/apps.py
+++ backend/hexa/user_management/apps.py
@@ -1,3 +1,5 @@
+from django.contrib import admin
+
 from hexa.app import CoreAppConfig
 
 
@@ -6,6 +8,25 @@ class UserManagementConfig(CoreAppConfig):
     label = "user_management"
 
     verbose_name = "User management"
+
+    def ready(self):
+        super().ready()
+        self._unregister_unused_allauth_admin()
+
+    @staticmethod
+    def _unregister_unused_allauth_admin():
+        """Hide allauth models that are unused or auto-managed in our SSO setup
+        (only SocialAccount stays registered).
+
+        Must run in ready(), after admin autodiscovery has imported allauth's
+        admin modules.
+        """
+        from allauth.account.models import EmailAddress, EmailConfirmation
+        from allauth.socialaccount.models import SocialApp, SocialToken
+
+        for model in (EmailAddress, EmailConfirmation, SocialApp, SocialToken):
+            if admin.site.is_registered(model):
+                admin.site.unregister(model)
 
     ANONYMOUS_URLS = [
         "logout",
