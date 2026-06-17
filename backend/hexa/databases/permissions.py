3e3204602b8ee2e8f925e3829abc7bd46175eb6e
YolanFery <yolan@codethefuture.be>
2026-06-17 10:53:38 +0200

    fix: permission

--- backend/hexa/databases/permissions.py
+++ backend/hexa/databases/permissions.py
@@ -8,3 +8,10 @@ def view_database_credentials(principal: User, workspace: Workspace):
         user=principal,
         role__in=[WorkspaceMembershipRole.EDITOR, WorkspaceMembershipRole.ADMIN],
     ).exists() or principal.is_organization_admin_or_owner(workspace.organization)
+
+
+def run_query(principal: User, workspace: Workspace):
+    """Any workspace member can run read-only SQL queries against the workspace database"""
+    return workspace.workspacemembership_set.filter(
+        user=principal
+    ).exists() or principal.is_organization_admin_or_owner(workspace.organization)
