f51910d36c761da45387d039a1ed85f1921867e7
YolanFery <yolan@codethefuture.be>
2026-07-17 16:44:41 +0200

    fix: test

--- backend/hexa/workspaces/tests/migrations/test_0061_grant_select_on_covid_data_to_ro_role.py
+++ backend/hexa/workspaces/tests/migrations/test_0061_grant_select_on_covid_data_to_ro_role.py
@@ -1,12 +1,17 @@
 from django.test import TransactionTestCase
+from psycopg2 import sql
 from psycopg2.errors import (
     InsufficientPrivilege,  # type: ignore[import-not-found] # psycopg2.errors is not in typeshed yet
 )
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 from hexa.core.test.migrator import Migrator
 from hexa.databases.api import delete_database
-from hexa.databases.tests.helpers import seed_demo_table_as_admin
-from hexa.databases.utils import get_workspace_database_ro_connection
+from hexa.databases.tests.helpers import seed_demo_table
+from hexa.databases.utils import (
+    get_workspace_database_connection,
+    get_workspace_database_ro_connection,
+)
 from hexa.files import storage
 from hexa.user_management.models import User
 from hexa.workspaces.models import Workspace
@@ -27,9 +32,16 @@ class Migration0061Test(TransactionTestCase):
         self.workspace = Workspace.objects.create_if_has_perm(
             self.user, name="Covid WS", description=""
         )
-        seed_demo_table_as_admin(
-            self.workspace, [(1, "Kinshasa")], table_name="covid_data"
-        )
+        seed_demo_table(self.workspace, [(1, "Kinshasa")], table_name="covid_data")
+        conn = get_workspace_database_connection(self.workspace)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        with conn.cursor() as cur:
+            cur.execute(
+                sql.SQL("REVOKE SELECT ON covid_data FROM {role};").format(
+                    role=sql.Identifier(self.workspace.db_ro_username)
+                )
+            )
+        conn.close()
 
     def tearDown(self):
         delete_database(self.workspace.db_name)
