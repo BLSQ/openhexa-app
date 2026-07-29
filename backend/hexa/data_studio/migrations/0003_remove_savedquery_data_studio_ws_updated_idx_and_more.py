a225d957cbe9ad1d0ebabe387185bf27afc3fa69
mrivar <martin7557@gmail.com>
2026-07-29 15:35:28 +0200

    fix lint

--- backend/hexa/data_studio/migrations/0003_remove_savedquery_data_studio_ws_updated_idx_and_more.py
+++ backend/hexa/data_studio/migrations/0003_remove_savedquery_data_studio_ws_updated_idx_and_more.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("data_studio", "0002_querylog"),
         ("workspaces", "0061_grant_select_on_covid_data_to_ro_role"),
