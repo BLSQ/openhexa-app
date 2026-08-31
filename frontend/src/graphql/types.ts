6cdc40f3e6e6849cde70a8cdb1d1884a964283e2
YolanFery <yolan@codethefuture.be>
2026-08-31 17:35:40 +0200

    fix: schedle unschedulable

--- frontend/src/graphql/types.ts
+++ frontend/src/graphql/types.ts
@@ -4232,7 +4232,6 @@ export type Pipeline = {
   functionalType?: Maybe<PipelineFunctionalType>;
   hasNewTemplateVersions: Scalars['Boolean']['output'];
   id: Scalars['UUID']['output'];
-  missingScheduleParameters: Array<Scalars['String']['output']>;
   name?: Maybe<Scalars['String']['output']>;
   newTemplateVersions: Array<PipelineTemplateVersion>;
   notebookPath?: Maybe<Scalars['String']['output']>;
